5f4aac82c8a07f3c48d3312341eda4cedeee101c
Remington Tyler Thornton <rtthorn@lanl.gov>
2020-03-24 10:38:19 -0600

    Updated example job creations script

--- jobScripts/createSLRUMscripts.py
+++ jobScripts/createSLRUMscripts.py
@@ -2,81 +2,146 @@
 
 import sys, os
 import subprocess
+from subprocess import PIPE
+import re
 import time
 import glob
 import datetime
+import argparse
+import sys
 
-# -------- settings ------------------------
+# -------- default settings ------------------------
 inputDir      = os.getenv("CCMRAW")+"/"
-outputDir     = os.getenv("CCMPROCESSED")+"/"
-defaultSLRUM  = os.getenv("LUSTRETMP")+"/findEvents_slrum.script"
-defaultXML    = os.getenv("CCMINSTALL")+"/configFiles/local/findEventsConfig.xml"
+outputDir     = os.getenv("CCMPROCESSED")+"/na22_20191221/"
+defaultSLRUM  = "findEvents_slrum.script"
+defaultXML    = "findEventsConfig.xml"
 hvOffList     = os.getenv("CCMINSTALL")+"/mappings/hv_off_2019.csv"
 calibrationFile = os.getenv("CCMINSTALL")+"/mappings/root_out_2019ledData_run179_legFix_integral_all_round4_.root"
 sourceFile     = os.getenv("HOME")+"/.bash_profile"
-logFileLoc     = os.getenv("CCMINSTALL")+"/log"
-runType       = "events"
+logFileLoc     = outputDir+"/log"
+jobNamePrefix  = "findEvents_"
+runType        = "events"
+rewrite        = False
+submit         = False
+currentUser    = os.getenv("USER")
+maxNumJobs     = 1000 # this is a hardcoded limit from LANL HPC
+inputDataRegex = "*run000193*.root"
+inputEndString = "_physics"
+
+def getOptions(args=sys.argv[1:]):
+  parser = argparse.ArgumentParser(description="Creates slrum and xml config file for the find events module. Submits the slrum script if --submit option is given.")
+  parser.add_argument("--input-loc", default=inputDir,
+      help="Directory location of the input files")
+  parser.add_argument("--output-loc", default=outputDir,
+      help="Directory location of the output files")
+  parser.add_argument("--default-slrum", default=defaultSLRUM,
+      help="Name of the default slrum script file")
+  parser.add_argument("--default-xml", default=defaultXML,
+      help="Name of the default xml script file")
+  parser.add_argument("--hv-off-list", default=hvOffList,
+      help="Path to the HV off csv file to use") 
+  parser.add_argument("--calibration-file", default=calibrationFile,
+      help="Path to the calibration ROOT file to use") 
+  parser.add_argument("--source-file", default=sourceFile,
+      help="Path to the source file to source in slrum script") 
+  parser.add_argument("--log-file", default=logFileLoc,
+      help="Path to the directory to save the log file. Default is a directory called log in the directory where the output files are saved")
+  parser.add_argument("--job-name-prefix", default=jobNamePrefix,
+      help="The prefix for the slurm job name")
+  parser.add_argument("--run-type", default=runType,
+      help="The postfix string to add to the output file")
+  parser.add_argument("--rewrite", default=rewrite, dest='rewrite', action='store_true',
+      help="Rewrite all files. Default is false.")
+  parser.add_argument("--submit", default=submit, dest='submit', action='store_true',
+      help="Submit the scripts to the queue. Default is false.")
+  parser.add_argument("--user", default=currentUser,
+      help="Name of user submitting the jobs")
+  parser.add_argument("--max-jobs", default=maxNumJobs, type=int,
+      help="The maximum number of jobs that can be submitted. Default is 1000 (HPC max)")
+  parser.add_argument("--input-data-regex", default=inputDataRegex,
+      help="The parameters to pass to ls to select the input files.")
+  parser.add_argument("--input-end", default=inputEndString,
+      help="The string that ends the input files (before the .root), used to get a unique name.")
+  parser.add_argument("--trigger-type", default="BEAM",
+      help="Which triggers to look at. See xml configuration file for possible options. Default = BEAM")
+  options = parser.parse_args(args)
+  return options
 
 # -------- main code ------------------------
-inputDir   = os.path.abspath(inputDir)
-outputDir  = os.path.abspath(outputDir)
+options = getOptions(sys.argv[1:])
+print(options)
+
+inputDir   = os.path.abspath(options.input_loc)
+outputDir  = os.path.abspath(options.output_loc)
 
 #  will need to change conditions on what to look for
-rawList  = glob.glob(os.path.join(inputDir, "*run000167*.root"))
+rawList  = glob.glob(os.path.join(inputDir, options.input_data_regex))
 
 slrumScripts = []
 
 for rawFile in rawList:
-  baseName = os.path.basename(rawFile.replace(".root", "_%s.root"%(runType)))
+  baseName = os.path.basename(rawFile.replace(".root", "_%s.root"%(options.run_type)))
   outName = outputDir+"/"+baseName
+
+  if os.path.exists(outName) and not options.rewrite:
+    continue
+
   start = baseName.find("run")
-  end = baseName.find("_physics")
+  end = baseName.find(inputEndString)
   run = baseName[start:end]
-  print("baseName",baseName," run ",run, " out ",outName)
+  jobName = options.job_name_prefix+run
 
-  slrumScript = outputDir+"/"+"findEvents_slrum_"+run+".script"
-  xmlScript = outputDir+"/"+"findEventsConfig_"+run+".xml"
+  #check to see if a job was already submitted for the file
+  out = subprocess.run(["squeue","-l","--name="+jobName], stdout=PIPE, universal_newlines=True)
+  numLines = len(re.findall("\w*"+options.user+"\w*",out.stdout))
+  if numLines > 0:
+    continue
 
-  fin = open(defaultSLRUM,"rt")
-  fout = open(slrumScript,"wt")
-  for line in fin:
-    copyLine = line
-    copyLine = copyLine.replace('CONFIGFILE',xmlScript)
-    copyLine = copyLine.replace('RUN',run)
-    copyLine = copyLine.replace('LOGFILELOCATION',logFileLoc)
-    copyLine = copyLine.replace('SOURCEFILE',sourceFile)
-    fout.write(copyLine)
+  print("run ",run, " out ",outName)
 
-  fin.close()
-  fout.close()
-
-  fin = open(defaultXML,"rt")
-  fout = open(xmlScript,"wt")
-  for line in fin:
-    copyLine = line
-    copyLine = copyLine.replace('INFILENAME',rawFile)
-    copyLine = copyLine.replace('OUTFILENAME',outName)
-    copyLine = copyLine.replace('HVOFFLIST',hvOffList)
-    copyLine = copyLine.replace('CALIBRATIONFILE',calibrationFile)
-    copyLine = copyLine.replace('TRIGGER',"BEAM")
-    fout.write(copyLine)
-
-  fin.close()
-  fout.close()
+  slrumScript = outputDir+"/scripts/"+"findEvents_slrum_"+run+".script"
+  xmlScript = outputDir+"/scripts/"+"findEventsConfig_"+run+".xml"
 
   slrumScripts.append(slrumScript)
 
-for slrum in slrumScripts:
-  subprocess.call(["sbatch",slrum])
+  if not os.path.exists(slrumScript) and options.rewrite:
+    print("\t\t\t replacing slrumScript")
 
+    fin = open(options.default_slrum,"rt")
+    fout = open(slrumScript,"wt")
+    for line in fin:
+      copyLine = line
+      copyLine = copyLine.replace('CONFIGFILE',xmlScript)
+      copyLine = copyLine.replace('JOBNAME',jobName)
+      copyLine = copyLine.replace('LOGFILELOCATION',options.log_file)
+      copyLine = copyLine.replace('SOURCEFILE',options.source_file)
+      fout.write(copyLine)
 
-"""
-fin = open("/lustre/scratch3/turquoise/rtthorn/findEvents_slrum.script", "rt")
-fout = open("/lustre/scratch3/turquoise/rtthorn/findEvents_slrum_specific.script", "wt")
+    fin.close()
+    fout.close()
 
-for line in fin:
-	fout.write(line.replace('CONFIGFILE', '$CCMINSTALL/configFiles/local/findEventsConfig.xml'))
-	
-fin.close()
-fout.close()
-"""
+  if not os.path.exists(xmlScript) and options.rewrite:
+    print("\t\t\t replacing xmlScript")
+    fin = open(options.default_xml,"rt")
+    fout = open(xmlScript,"wt")
+    for line in fin:
+      copyLine = line
+      copyLine = copyLine.replace('INFILENAME',rawFile)
+      copyLine = copyLine.replace('OUTFILENAME',outName)
+      copyLine = copyLine.replace('HVOFFLIST',options.hv_off_list)
+      copyLine = copyLine.replace('CALIBRATIONFILE',options.calibration_file)
+      copyLine = copyLine.replace('TRIGGER',options.trigger_type)
+      fout.write(copyLine)
+
+    fin.close()
+    fout.close()
+
+out = subprocess.run(["squeue","-l", "-u", options.user], stdout=PIPE, universal_newlines=True)
+numLines = len(re.findall("\w*"+options.user+"\w*",out.stdout))
+
+print("MaxNumJobs ",maxNumJobs," numLines ",numLines," numScripts ",len(slrumScripts))
+
+if options.submit:
+  n = min(maxNumJobs-numLines,len(slrumScripts))
+  for slrum in slrumScripts[:n]:
+    subprocess.call(["sbatch",slrum])
