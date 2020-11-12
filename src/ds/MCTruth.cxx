b53d5bd51c075e1c9fa8f32871fe365ed5bd6b7a
Remington Tyler Thornton <rtthorn@lanl.gov>
2020-11-12 16:02:29 -0700

    Forgot to add these files

---
+++ src/ds/MCTruth.cxx
@@ -0,0 +1,272 @@
+/*!**********************************************
+ * \file MCTruth.cxx
+ * \brief Source code for the #MCTruth class
+ * \author R. T. Thornton (LANL)
+ * \date November 11, 2020
+ ***********************************************/
+#include "MCTruth.h"
+#include "MsgLog.h"
+
+#include <memory>
+#include <algorithm>
+#include <numeric>
+
+ClassImp(MCTruth)
+
+/*!**********************************************
+ * \fn MCTruth::MCTruth()
+ * \brief Default Constructor
+ ***********************************************/
+MCTruth::MCTruth() : TObject()
+{
+	Reset(0);
+}
+
+/*!**********************************************
+ * \fn MCTruth::MCTruth(int particleID)
+ * \brief Constructor (calls #Reset)
+ * \param[in] particleID The PDG number for the particle that was simulated
+ ***********************************************/
+MCTruth::MCTruth(int particleID)
+: TObject()
+{
+  Reset(particleID);
+}
+
+/*!**********************************************
+ * \fn void MCTruth::Reset(int particleID)
+ * \brief Resets the values to what is passed
+ * \param[in] particleID The PDG number for the particle that was simulated
+ ***********************************************/
+void MCTruth::Reset(int particleID)
+{
+	fParticleID = particleID;
+	fMomX = 0;
+	fPosX = 0;
+	fMomY = 0;
+	fPosY = 0;
+	fMomZ = 0;
+	fPosZ = 0;
+
+	fHitEnergy.clear();
+	fHitAngle.clear();
+	fHitUncoated.clear();
+	fHitRow.clear();
+	fHitCol.clear();
+	fHitTime.clear();
+}
+
+/*!**********************************************
+ * \fn MCTruth::MCTruth(const MCTruth & rhs)
+ * \brief Copy constructor
+ * \param[in] rhs The object to copy
+ ***********************************************/
+MCTruth::MCTruth(const MCTruth & rhs) : TObject(rhs)
+{
+  this->operator=(rhs);
+}
+
+/*!**********************************************
+ * \fn MCTruth::~MCTruth()
+ * \brief Destructor
+ ***********************************************/
+MCTruth::~MCTruth()
+{
+  // empty
+	fHitEnergy.clear();
+	fHitAngle.clear();
+	fHitUncoated.clear();
+	fHitRow.clear();
+	fHitCol.clear();
+	fHitTime.clear();
+}
+
+/*!**********************************************
+ * \fn void MCTruth::SetParticleMomentum(float x, float y, float z)
+ * \brief Sets the particles initial momentum 
+ * \param[in] x The x coordinate 
+ * \param[in] y The Y coordinate
+ * \param[in] z The Z coordinate
+ ***********************************************/
+void MCTruth::SetParticleMomentum(float x, float y, float z)
+{
+	fMomX = x;
+	fMomY = y;
+	fMomZ = z;
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::SetParticlePosition(float x, float y, float z)
+ * \brief Sets the particles initial position
+ * \param[in] x The x coordinate 
+ * \param[in] y The Y coordinate
+ * \param[in] z The Z coordinate
+ ***********************************************/
+void MCTruth::SetParticlePosition(float x, float y , float z)
+{
+	fPosX = x;
+	fPosY = y;
+	fPosZ = z;
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::AddHitInformation(int row, int col, bool uncoated, float energy, float time, float angle)
+ * \brief Adds new hit information to the current vectors
+ * \param[in] row The row number
+ * \param[in] col The column number
+ * \param[in] uncoated A flag saying if the PMT is coated or uncoated
+ * \param[in] energy The energy of the hit in eV
+ * \param[in] time The time of the hit in ns
+ * \param[in] angle The angle the PMT was hit
+ ***********************************************/
+void MCTruth::AddHitInformation(int row, int col, bool uncoated, float energy, float time, float angle)
+{
+	fHitRow.emplace_back(row);
+	fHitCol.emplace_back(col);
+	fHitUncoated.emplace_back(uncoated);
+	fHitEnergy.emplace_back(energy);
+	fHitTime.emplace_back(time);
+	fHitAngle.emplace_back(angle);
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::SetHitInformation(size_t hitIndex, int row, int col, bool uncoated, float energy, float time, float angle)
+ * \brief Sets the hit information at the passed hit index
+ * \param[in] hitIndex The index to save the information to
+ * \param[in] row The row number
+ * \param[in] col The column number
+ * \param[in] uncoated A flag saying if the PMT is coated or uncoated
+ * \param[in] energy The energy of the hit in eV
+ * \param[in] time The time of the hit in ns
+ * \param[in] angle The angle the PMT was hit
+ *
+ * If hitIndex is greater than the current size of the vectors
+ * the information will be appended to the vectors and an
+ * error message will be printed
+ ***********************************************/
+void MCTruth::SetHitInformation(size_t hitIndex, int row, int col, bool uncoated, float energy, float time, float angle)
+{
+	if (hitIndex >= fHitRow.size()) {
+		MsgError(MsgLog::Form("The index passed %zu is greater than current size %zu. Appending the hit information",
+					hitIndex,fHitRow.size()));
+		AddHitInformation(row,col,uncoated,energy,time,angle);
+		return;
+	}
+
+	fHitRow.at(hitIndex) = row;
+	fHitCol.at(hitIndex) = col;
+	fHitUncoated.at(hitIndex) = uncoated;
+	fHitEnergy.at(hitIndex) = energy;
+	fHitTime.at(hitIndex) = time;
+	fHitAngle.at(hitIndex) = angle;
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::SetNumberOfHits(size_t numHits)
+ * \brief Sets the hit information vectors to the size passed
+ * \param[in] numHits The size to set the vectors to
+ ***********************************************/
+void MCTruth::SetNumberHits(size_t numHits)
+{
+	fHitRow.resize(numHits);
+	fHitCol.resize(numHits);
+	fHitUncoated.resize(numHits);
+	fHitEnergy.resize(numHits);
+	fHitTime.resize(numHits);
+	fHitAngle.resize(numHits);
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::GetParticleMomentum(float & x, float & y, flato & z)
+ * \brief Gets the particles initial momentum 
+ * \param[out] x The x coordinate 
+ * \param[out] y The Y coordinate
+ * \param[out] z The Z coordinate
+ ***********************************************/
+void MCTruth::GetParticleMomentum(float & x, float & y , float & z)
+{
+	x = fMomX;
+	y = fMomY;
+	z = fMomZ;
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::GetParticlePosition(float & x, float & y, flato & z)
+ * \brief Gets the particles initial position 
+ * \param[out] x The x coordinate 
+ * \param[out] y The Y coordinate
+ * \param[out] z The Z coordinate
+ ***********************************************/
+void MCTruth::GetParticlePosition(float & x, float & y , float & z)
+{
+	x = fPosX;
+	y = fPosY;
+	z = fPosZ;
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::GetParticleMomentum(float & x, float & y, flato & z) const
+ * \brief Gets the particles initial momentum 
+ * \param[out] x The x coordinate 
+ * \param[out] y The Y coordinate
+ * \param[out] z The Z coordinate
+ ***********************************************/
+void MCTruth::GetParticleMomentum(float & x, float & y , float & z) const
+{
+	x = fMomX;
+	y = fMomY;
+	z = fMomZ;
+	return;
+}
+
+/*!**********************************************
+ * \fn void MCTruth::GetParticlePosition(float & x, float & y, flato & z) const
+ * \brief Gets the particles initial position 
+ * \param[out] x The x coordinate 
+ * \param[out] y The Y coordinate
+ * \param[out] z The Z coordinate
+ ***********************************************/
+void MCTruth::GetParticlePosition(float & x, float & y , float & z) const
+{
+	x = fPosX;
+	y = fPosY;
+	z = fPosZ;
+	return;
+}
+
+/*!**********************************************
+ * \fn MCTruth & MCTruth::operator=(const MCTruth & rhs)
+ * \brief Copy assignment for the #MCTruth class
+ * \return Return reference to the current class
+ ***********************************************/
+MCTruth & MCTruth::operator=(const MCTruth & rhs)
+{
+
+	this->fParticleID = rhs.fParticleID;
+	this->fEnergyDeposited = rhs.fEnergyDeposited;
+	this->fMomX = rhs.fMomX;
+	this->fMomY = rhs.fMomY;
+	this->fMomZ = rhs.fMomZ;
+	this->fPosX = rhs.fPosX;
+	this->fPosY = rhs.fPosY;
+	this->fPosZ = rhs.fPosZ;
+
+	this->fHitEnergy = rhs.fHitEnergy;
+	this->fHitTime = rhs.fHitTime;
+	this->fHitRow = rhs.fHitRow;
+	this->fHitCol = rhs.fHitCol;
+	this->fHitUncoated = rhs.fHitUncoated;
+	this->fHitAngle = rhs.fHitAngle;
+
+  return *this;
+}
+
+
+
---
+++ src/ds/MCTruth.h
@@ -0,0 +1,113 @@
+/*!**********************************************
+ * \file MCTruth.h
+ * \brief Header file for the #MCTruth class
+ * \author R. T. Thornton (LANL)
+ * \date November 11, 2020
+ ***********************************************/
+#ifndef MCTruth_h
+#define MCTruth_h
+
+#include <vector>
+#include <iostream>
+#include "TObject.h"
+
+/*!**********************************************
+ * \class MCTruth
+ * \brief Container for the raw data coming out for the detector
+ ***********************************************/
+class MCTruth : public TObject
+{
+  public:
+    MCTruth();
+    MCTruth(int particleID);
+    MCTruth(const MCTruth & rhs);
+    ~MCTruth();
+
+    void Reset(int particleID);
+
+		void SetParticleID(int particleID) { fParticleID = particleID; }
+		void SetTotalEnergyDeposited(float energy) { fEnergyDeposited = energy; }
+		void SetParticleMomentum(float x, float y, float z);
+		void SetParticlePosition(float x, float y, float z);
+		void AddHitInformation(int row, int col, bool uncoated, float energy, float time, float angle);
+		void SetHitInformation(size_t hitIndex, int row, int col, bool uncoated, float energy, float time, float angle);
+		void SetNumberHits(size_t hitIndex);
+
+		int GetParticleID() { return fParticleID; }
+		float GetTotalEnergyDeposited() { return fEnergyDeposited; }
+		float GetParticleMomentumX() { return fMomX; }
+		float GetParticleMomentumY() { return fMomY; }
+		float GetParticleMomentumZ() { return fMomZ; }
+		float GetParticlePositionX() { return fPosX; }
+		float GetParticlePositionY() { return fPosY; }
+		float GetParticlePositionZ() { return fPosZ; }
+		void GetParticleMomentum(float & x, float & y , float & z);
+		void GetParticlePosition(float & x, float & y , float & z);
+
+		size_t GetHitNumber() { return fHitRow.size(); }
+		int GetHitRow(size_t index) { return fHitRow.at(index); }
+		int GetHitCol(size_t index) { return fHitCol.at(index); }
+		bool GetHitUncoated(size_t index) { return fHitUncoated.at(index); }
+		float GetHitEnergy(size_t index) { return fHitEnergy.at(index); }
+		float GetHitAngle(size_t index) { return fHitAngle.at(index); }
+		float GetHitTime(size_t index) { return fHitTime.at(index); }
+
+		int GetParticleID() const { return fParticleID; }
+		float GetTotalEnergyDeposited() const { return fEnergyDeposited; }
+		float GetParticleMomentumX() const { return fMomX; }
+		float GetParticleMomentumY() const { return fMomY; }
+		float GetParticleMomentumZ() const { return fMomZ; }
+		float GetParticlePositionX() const { return fPosX; }
+		float GetParticlePositionY() const { return fPosY; }
+		float GetParticlePositionZ() const { return fPosZ; }
+		void GetParticleMomentum(float & x, float & y , float & z) const;
+		void GetParticlePosition(float & x, float & y , float & z) const;
+
+		size_t GetHitNumber() const { return fHitRow.size(); }
+		int GetHitRow(size_t index) const { return fHitRow.at(index); }
+		int GetHitCol(size_t index) const { return fHitCol.at(index); }
+		bool GetHitUncoated(size_t index) const { return fHitUncoated.at(index); }
+		float GetHitEnergy(size_t index) const { return fHitEnergy.at(index); }
+		float GetHitAngle(size_t index) const { return fHitAngle.at(index); }
+		float GetHitTime(size_t index) const { return fHitTime.at(index); }
+
+    MCTruth & operator=(const MCTruth & rhs);
+
+  private:
+
+		/// initial particle id using pdg encoding
+		int fParticleID;
+
+    /// Total Energy Deposited in keV
+    float fEnergyDeposited;
+    /// X Momentum
+    float fMomX;
+    /// Y Momentum
+    float fMomY;
+    /// Z Momentum
+    float fMomZ;
+    /// X Position
+    float fPosX;
+    /// Y Position
+    float fPosY;
+    /// Z Position
+    float fPosZ;
+
+		/// The energy of every hit in eV
+		std::vector<float> fHitEnergy;
+		/// The time of every hit in ns
+		std::vector<float> fHitTime;
+		/// The row number of the PMT that was hit
+		std::vector<int> fHitRow;
+		/// The column number of the PMT that was hit
+		std::vector<int> fHitCol;
+		/// A flag if the PMT is coated or uncoated
+		std::vector<bool> fHitUncoated;
+		/// The angle at which the PMT was hit
+		std::vector<float> fHitAngle;
+
+  ClassDef(MCTruth,1)
+};
+
+#endif // MCTruth_h
+
