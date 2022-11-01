5984542014484a5dbab532eaced41d92e1574577
Austin Schneider <aschneider@lanl.gov>
2022-11-01 13:09:21 -0600

    I3 merge all_tools.cmake

--- cmake/all_tools.cmake
+++ cmake/all_tools.cmake
@@ -1,15 +1,13 @@
 #
 #  These guys are in subdirectory 'tools'
 #
-set(ALL_TOOLS root geant4 xercesc argagg
-    ${CCM_EXTRA_TOOLS}   # add the extra tools and dedupe
+set(ALL_TOOLS pthread python boost boostnumpy root
+    xercesc argagg
+    blas gsl sprng pal libarchive
+    suitesparse ncurses cdk
+    cfitsio healpix hdf5 minuit2 clhep geant4 zlib
+    opencl cuda gmp zmq
+    multinest nlopt zstd fftw3 photospline
+    ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
     )
-# set(ALL_TOOLS pthread python boost boostnumpy root 
-#     blas gsl sprng pal libarchive
-#     suitesparse ncurses cdk
-#     cfitsio healpix hdf5 minuit2 clhep geant4 zlib
-#     opencl cuda gmp zmq
-#     multinest nlopt zstd fftw3 photospline
-#     ${I3_EXTRA_TOOLS}   # add the extra tools and dedupe
-#     )
 list(REMOVE_DUPLICATES ALL_TOOLS)
