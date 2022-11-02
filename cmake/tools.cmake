6665a3ef63dc4395868d0f6889c9a0f22820e814
Austin Schneider <aschneider@lanl.gov>
2022-11-02 14:21:34 -0600

    I3 merge tools.cmake

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -12,7 +12,7 @@
 #  2. Redistributions in binary form must reproduce the above copyright
 #     notice, this list of conditions and the following disclaimer in the
 #     documentation and/or other materials provided with the distribution.
-#  
+#
 #  THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
 #  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 #  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -24,9 +24,9 @@
 #  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 #  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 #  SUCH DAMAGE.
-#  
+#
 #  SPDX-License-Identifier: BSD-2-Clause
-#  
+#
 #
 colormsg("")
 colormsg(_HIBLUE_ "Configuring tools...")
@@ -38,39 +38,39 @@ add_custom_target(install_tool_libs)
 include(tooldef)
 
 #
-#  By default, use /usr/share/fizzicks/cmake as CCM_SITE_CMAKE_DIR
+#  By default, use /usr/share/fizzicks/cmake as I3_SITE_CMAKE_DIR
 #
-if (NOT IS_DIRECTORY $ENV{CCM_SITE_CMAKE_DIR})
-  set (CCM_SITE_CMAKE_DIR "/usr/share/fizzicks/cmake"
+if (NOT IS_DIRECTORY $ENV{I3_SITE_CMAKE_DIR})
+  set (I3_SITE_CMAKE_DIR "/usr/share/fizzicks/cmake"
     CACHE PATH "Path to site-specific cmake files")
-  message(STATUS "Using default site cmake dir of ${CCM_SITE_CMAKE_DIR}")
+  message(STATUS "Using default site cmake dir of ${I3_SITE_CMAKE_DIR}")
 else()
-  set (CCM_SITE_CMAKE_DIR $ENV{CCM_SITE_CMAKE_DIR}
+  set (I3_SITE_CMAKE_DIR $ENV{I3_SITE_CMAKE_DIR}
     CACHE PATH "Path to site-specific cmake files")
-  message(STATUS "Using user-configured CCM_SITE_CMAKE_DIR=${CCM_SITE_CMAKE_DIR}")
+  message(STATUS "Using user-configured I3_SITE_CMAKE_DIR=${I3_SITE_CMAKE_DIR}")
 endif()
 
 include(all_tools)
 #
 #  Load each tool, directory preferences are, in order:
-#  * CCM_SITE_CMAKE_DIR
-#  * CCM_EXTRA_TOOLS_DIR
+#  * I3_SITE_CMAKE_DIR
+#  * I3_EXTRA_TOOLS_DIR
 #  * CMAKE_SOURCE_DIR/cmake/tools
 #
 foreach(tool ${ALL_TOOLS})
 
-  if (EXISTS ${CCM_SITE_CMAKE_DIR}/${tool}.cmake)
+  if (EXISTS ${I3_SITE_CMAKE_DIR}/${tool}.cmake)
     message(STATUS "")
     message(STATUS "Reading ${tool} from site cmake dir")
-    include(${CCM_SITE_CMAKE_DIR}/${tool}.cmake)
-  elseif(EXISTS "${CCM_EXTRA_TOOLS_DIR}/${tool}.cmake")
+    include(${I3_SITE_CMAKE_DIR}/${tool}.cmake)
+  elseif(EXISTS "${I3_EXTRA_TOOLS_DIR}/${tool}.cmake")
     message(STATUS "")
     message(STATUS "Reading ${tool} from extra tools dir")
-    include(${CCM_EXTRA_TOOLS_DIR}/${tool}.cmake)
+    include(${I3_EXTRA_TOOLS_DIR}/${tool}.cmake)
   elseif(EXISTS ${CMAKE_SOURCE_DIR}/cmake/tools/${tool}.cmake)
     include(${CMAKE_SOURCE_DIR}/cmake/tools/${tool}.cmake)
-  else(EXISTS ${CCM_SITE_CMAKE_DIR}/${tool}.cmake)
-    message(STATUS "*** ${tool}.cmake not found in CCM_SITE_CMAKE_DIR, CCM_EXTRA_TOOLS_DIR, or cmake/tools")
+  else(EXISTS ${I3_SITE_CMAKE_DIR}/${tool}.cmake)
+    message(STATUS "*** ${tool}.cmake not found in I3_SITE_CMAKE_DIR, I3_EXTRA_TOOLS_DIR, or cmake/tools")
     message(STATUS "*** This is only an error if a projects requires tool '${tool}'")
   endif()
 
@@ -129,7 +129,7 @@ macro(use_tools THIS_USE_TOOLS_TARGET)
   foreach(USED_TOOL ${${THIS_USE_TOOLS_TARGET}_ALL_TOOLS})
     use_tool(${THIS_USE_TOOLS_TARGET} ${USED_TOOL})
   endforeach(USED_TOOL ${${THIS_USE_TOOLS_TARGET}_ALL_TOOLS})
-  
+
   if(NOT ${THIS_USE_TOOLS_TARGET}_COMPILE_FLAGS)
     set(${THIS_USE_TOOLS_TARGET}_COMPILE_FLAGS "")
   else(NOT ${THIS_USE_TOOLS_TARGET}_COMPILE_FLAGS)
@@ -138,9 +138,9 @@ macro(use_tools THIS_USE_TOOLS_TARGET)
 
   get_property(${THIS_USE_TOOLS_TARGET}_INCLUDE_DIRS DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
   set(${THIS_USE_TOOLS_TARGET}_INCLUDE_DIRS ${${THIS_USE_TOOLS_TARGET}_INCLUDE_DIRS} CACHE INTERNAL "Directories included by library ${THIS_USE_TOOLS_TARGET}")
-  
+
   set(${THIS_USE_TOOLS_TARGET}_COMPILE_FLAGS ${${THIS_USE_TOOLS_TARGET}_COMPILE_FLAGS} CACHE INTERNAL "Compilation flags used by library ${THIS_USE_TOOLS_TARGET}")
-  
+
   set(${THIS_USE_TOOLS_TARGET}_LIBRARY_DEPENDS ${${THIS_USE_TOOLS_TARGET}_LIBRARIES} CACHE INTERNAL "Libraries against which ${THIS_USE_TOOLS_TARGET} links")
 
 endmacro(use_tools)
