79686db2cb935092fa4c3c9c4018b15da6c4ed7c
Austin Schneider <aschneider@lanl.gov>
2022-11-02 14:31:12 -0600

    Spacing for utility/filter_out.cmake

--- cmake/utility/filter_out.cmake
+++ cmake/utility/filter_out.cmake
@@ -1,19 +1,19 @@
 MACRO(FILTER_OUT FILTERS INPUTS OUTPUT)
-  # Mimicks Gnu Make's $(filter-out) which removes elements 
+  # Mimicks Gnu Make's $(filter-out) which removes elements
   # from a list that match the pattern.
   # Arguments:
   #  FILTERS - list of patterns that need to be removed
   #  INPUTS  - list of inputs that will be worked on
   #  OUTPUT  - the filtered list to be returned
-  # 
-  # Example: 
+  #
+  # Example:
   #  SET(MYLIST this that and the other)
   #  SET(FILTS this that)
   #
   #  FILTER_OUT("${FILTS}" "${MYLIST}" OUT)
   #  MESSAGE("OUTPUT = ${OUT}")
   #
-  # The output - 
+  # The output -
   #   OUTPUT = and;the;other
   #
   SET(FOUT "")
@@ -21,9 +21,9 @@ MACRO(FILTER_OUT FILTERS INPUTS OUTPUT)
     SET(FILTERED 0)
     FOREACH(FILT ${FILTERS})
       IF(${FILTERED} EQUAL 0)
-    # MESSAGE("FILT=${FILT} INP=${INP}")
+        # MESSAGE("FILT=${FILT} INP=${INP}")
         IF("${INP}" MATCHES "${FILT}")
-      # MESSAGE("BYE BYE")
+          # MESSAGE("BYE BYE")
           SET(FILTERED 1)
         ENDIF("${INP}" MATCHES "${FILT}")
       ENDIF(${FILTERED} EQUAL 0)
@@ -38,21 +38,21 @@ ENDMACRO(FILTER_OUT FILTERS INPUTS OUTPUT)
 
 
 MACRO(STREQUAL_FILTER_OUT FILTERS INPUTS OUTPUT)
-  # Mimicks Gnu Make's $(filter-out) which removes elements 
+  # Mimicks Gnu Make's $(filter-out) which removes elements
   # from a list that match the pattern.
   # Arguments:
   #  FILTERS - list of patterns that need to be removed
   #  INPUTS  - list of inputs that will be worked on
   #  OUTPUT  - the filtered list to be returned
-  # 
-  # Example: 
+  #
+  # Example:
   #  SET(MYLIST this that and the other)
   #  SET(FILTS this that)
   #
   #  FILTER_OUT("${FILTS}" "${MYLIST}" OUT)
   #  MESSAGE("OUTPUT = ${OUT}")
   #
-  # The output - 
+  # The output -
   #   OUTPUT = and;the;other
   #
   SET(FOUT "")
@@ -60,9 +60,9 @@ MACRO(STREQUAL_FILTER_OUT FILTERS INPUTS OUTPUT)
     SET(FILTERED 0)
     FOREACH(FILT ${FILTERS})
       IF(${FILTERED} EQUAL 0)
-    # MESSAGE("FILT=${FILT} INP=${INP}")
+        # MESSAGE("FILT=${FILT} INP=${INP}")
         IF("${INP}" STREQUAL "${FILT}")
-      # MESSAGE("BYE BYE")
+          # MESSAGE("BYE BYE")
           SET(FILTERED 1)
         ENDIF("${INP}" STREQUAL "${FILT}")
       ENDIF(${FILTERED} EQUAL 0)
