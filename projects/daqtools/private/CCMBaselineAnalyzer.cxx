d69262bdd38f8942c612c631162151fa4f1f48e0
Austin Schneider <aschneider@lanl.gov>
2023-02-10 14:09:43 -0700

    Fixes to waveform processing utilities

--- projects/daqtools/private/CCMBaselineAnalyzer.cxx
+++ projects/daqtools/private/CCMBaselineAnalyzer.cxx
@@ -37,18 +37,18 @@ class SourceExhausted {};
 template<class T>
 class Source {
 public:
-    virtual T output();
+    virtual T output() = 0;
 };
 
 template<class T>
-class WaveformSource : virtual public Source<T> {
+class WaveformSource : public Source<T> {
 protected:
     std::vector<T> const & source;
     size_t current_pos;
 public:
     WaveformSource(CCMWaveform<T> const & source_wf) :
         source(source_wf.GetWaveform()), current_pos(0) {}
-    virtual inline T output() {
+    inline T output() override {
         if(current_pos < source.size()) {
             T const & x = source[current_pos];
             current_pos += 1;
@@ -61,67 +61,73 @@ public:
 };
 
 template<class T, class U>
-class Filter : virtual public Source<U> {
+class Filter : public Source<U> {
 protected:
     Source<T> * input;
 public:
     Filter() {}
     Filter(Source<T> * input) : input(input) {}
-    virtual inline U filter(T const & t);
-    virtual inline U output() {
+    virtual inline U filter(T const & t) = 0;
+    inline U output() override {
         return filter(input->output());
     }
 };
 
 template<class T, class U>
-class ExponentialFilter : virtual public Filter<T, U> {
+class ExponentialFilter : public Filter<T, U> {
 protected:
     U tau;
     U delta_t;
     U y_i;
     U alpha;
+    bool init;
 public:
-    ExponentialFilter(U tau, U delta_t, U y_0, Source<T> * input) :
-        tau(tau), delta_t(delta_t), y_i(y_0), Filter<T, U>(input) {
+    ExponentialFilter(U tau, U delta_t, Source<T> * input) :
+        tau(tau), delta_t(delta_t), init(true), Filter<T, U>(input) {
         alpha = exp(-delta_t / tau);
     }
 
-    virtual inline U filter(T const & t) {
-        y_i = y_i + (1.0 - alpha) * (t - y_i);
+    inline U filter(T const & t) override {
+        if(init) {
+            y_i = t;
+            init = false;
+        }
+        y_i += (1.0 - alpha) * (t - y_i);
         return y_i;
     }
 };
 
 template<class T, class U>
-class MultiplicativeFilter : virtual public Filter<T, U> {
+class MultiplicativeFilter : public Filter<T, U> {
 protected:
     U c;
 public:
     MultiplicativeFilter(U c, Source<T> * input) :
         c(c), Filter<T, U>(input) {}
-    virtual inline U filter(T const & t) {
-        return U(t)*c;
+    inline U filter(T const & t) override {
+        U x = U(t)*c;
+        return x;
     }
 };
 
 template<class T, class U>
-class Buffer : virtual public Filter<T, U> {
+class Buffer : public Filter<T, U> {
 protected:
     std::deque<T> buffer;
     size_t max_size;
     bool buffer_filling;
     bool source_exhausted;
 public:
-    Buffer(std::deque<T> init_buffer, size_t max_size, Source<T> * input) :
-        buffer(init_buffer), max_size(max_size), buffer_filling(true), source_exhausted(false), Filter<T, U>(input) {
+    Buffer(std::deque<T> init_buffer, size_t max_size, Source<T> * source_input) :
+        buffer(init_buffer), max_size(max_size), buffer_filling(true), source_exhausted(false), Filter<T, U>(source_input) {
         if(buffer.size() >= max_size)
             buffer_filling = false;
-        size_t to_pop = buffer.size() - std::max(max_size, buffer.size());
+        size_t to_pop = buffer.size() - std::min(max_size, buffer.size());
         for(size_t i=0; i<to_pop; ++i) {
             buffer.pop_front();
         }
     }
-    virtual inline U output() {
+    inline U output() override {
         if(buffer_filling) {
             try {
                 T t = Filter<T, U>::input->output();
@@ -151,11 +157,11 @@ public:
         }
         return filter(buffer);
     }
-    virtual inline U filter(std::deque<T> const & buffer);
+    virtual inline U filter(std::deque<T> const & buffer) = 0;
 };
 
 template<class T, class U>
-class BufferView : virtual public Filter<T, U> {
+class BufferView : public Filter<T, U> {
 protected:
     std::deque<T> buffer;
     size_t buffer_begin;
@@ -165,7 +171,8 @@ public:
         buffer_begin = 0;
         buffer_end = 0;
     }
-    virtual inline U filter(T const & t) {
+
+    inline U filter(T const & t) override {
         buffer.push_back(t);
         buffer_end += 1;
         return t;
@@ -186,7 +193,7 @@ public:
 };
 
 template<class T, class U>
-class SmoothingFilter : virtual public Buffer<T, U> {
+class SmoothingFilter : public Buffer<T, U> {
 protected:
     size_t zero_pos;
     std::deque<U> coeff;
@@ -201,7 +208,8 @@ public:
         for(size_t i=0; i<n_preload; ++i)
             Buffer<T, U>::buffer.push_back(input->output());
     }
-    virtual inline U filter(std::deque<T> const & buffer) {
+    inline U filter(std::deque<T> const & buffer) override {
+        U x;
         if(Buffer<T, U>::buffer_filling) {
             size_t n_excluded = coeff.size() - buffer.size();
             U this_total = 0;
@@ -211,10 +219,10 @@ public:
                 this_total += coeff[j];
                 sum += coeff[j] * buffer[i];
             }
-            if(this_total == 0) {
-                return 0;
-            }
-            return sum / this_total;
+            if(this_total == 0)
+                x = 0;
+            else
+                x = sum / this_total;
         } else if(Buffer<T, U>::source_exhausted) {
             if(buffer.size() <= zero_pos)
                 throw SourceExhausted();
@@ -224,40 +232,67 @@ public:
                 this_total += coeff[i];
                 sum += coeff[i] * buffer[i];
             }
-            if(this_total == 0) {
-                return 0;
-            }
-            return sum / this_total;
-
+            if(this_total == 0)
+                x = 0;
+            else
+                x = sum / this_total;
         } else {
             U sum = 0;
             for(size_t i=0; i<coeff.size(); ++i) {
                 sum += coeff[i] * buffer[i];
             }
-            return sum / total;
+            x = sum / total;
         }
+        return x;
+    }
+    inline U filter(T const & t) override {return t;};
+};
+
+template<class T, class U>
+class BackDerivativeFilter : public Filter<T, U> {
+protected:
+    U bin_width;
+    U t_last;
+    bool init;
+public:
+    BackDerivativeFilter(U bin_width, Source<T> * input) :
+        bin_width(bin_width), init(true), Filter<T, U>(input) {}
+    inline U filter(T const & t) override {
+        U x;
+        if(init) {
+            x = 0;
+            init = false;
+        } else {
+            x = (t - t_last) / (bin_width);
+        }
+        t_last = t;
+        return x;
     }
 };
 
 template<class T, class U>
-class DerivativeFilter : virtual public Filter<T, U> {
+class FrontDerivativeFilter : public Filter<T, U> {
 protected:
     U bin_width;
-    size_t n_seen;
-    U t_m2;
-    U t_m1;
+    U t_last;
+    bool ending;
+    bool end;
 public:
-    DerivativeFilter(U bin_width, Source<T> * input) :
-        bin_width(bin_width), n_seen(0), Filter<T, U>(input) {}
-    virtual inline U filter(T const & t) {
+    FrontDerivativeFilter(U bin_width, Source<T> * input) :
+        bin_width(bin_width), ending(false), end(false), Filter<T, U>(input) {
+        t_last = Filter<T, U>::input->output();
+    }
+    inline U filter(T const & t) override {
         U x;
-        if(n_seen >= 3) {
-            x = (t - t_m2) / (bin_width * 2.0);
-        } else {
+        if(end) {
+            throw SourceExhausted();
+        } else if(ending) {
             x = 0;
+            end = true;
+        } else {
+            x = (t - t_last) / (bin_width);
         }
-        t_m2 = t_m1;
-        t_m1 = t;
+        t_last = t;
         return x;
     }
 };
@@ -305,7 +340,7 @@ struct DerivativeWindowCapture {
     // 0 - outside of region without activity
     // 1 - inside of region without activity
 
-    DerivativeWindowCapture(Source<K> & d_buffer, BufferView<T, U> raw_buffer, K max_derivative) :
+    DerivativeWindowCapture(Source<K> & d_buffer, BufferView<T, U> & raw_buffer, K max_derivative) :
         d_buffer(d_buffer), raw_buffer(raw_buffer), max_derivative(max_derivative), state(0) {}
 
     bool Inside(T const & t) const {
@@ -315,24 +350,26 @@ struct DerivativeWindowCapture {
     bool Next() {
         K derivative = d_buffer.output();
         bool inside = Inside(derivative);
+        // std::cout << "Currently inside (" << (state ? "True" : "False") << ") will be (" << (inside ? "True" : "False") << ") with derivative = " << derivative << std::endl;
+        bool found_region = false;
         if(state == 0) { // currently outside region
             if(inside) { // moving inside region
                 raw_buffer.Reset();
-                return false;
             } else { // still outside region
-                return false;
             }
         } else { // already inside
             if(not inside) { // moving outside region
-                return true;
+                found_region = true;
             } else { // still inside region
-                return false;
             }
         }
+        state = inside;
+        return found_region;
     }
 
-    std::deque<K> const & GetBuffer() const {
-        return raw_buffer.GetBuffer();
+    std::deque<K> GetBuffer() const {
+        std::deque<K> limited_buffer(raw_buffer.GetBuffer().begin(), raw_buffer.GetBuffer().end()-1);
+        return limited_buffer;
     }
 };
 
@@ -342,40 +379,40 @@ struct WindowStats {
     double k_;
     double M_;
     double S_;
-    WindowStats() {}
+
     template<typename T>
-    WindowStats(std::deque<T> const & buffer) {
-        double k = 0;
-        double M = 0;
-        double S = 0;
-        for(size_t i=0; i<buffer.size(); ++i) {
-            k += 1;
-            double const & x = buffer[i];
-            double new_M = M + (x + M)* 1.0 / k;
-            double new_S = S + (x - M) * (x - new_M);
-            M = new_M;
-            S = new_S;
-        }
-        mean = M;
-        variance = S / (k - 1);
-        k_ = k; M_ = M; S_ = M;
+    void AddSample(T const & x) {
+        k_ += 1;
+        double delta = x - M_;
+        M_ += delta / k_;
+        double delta2 = x - M_;
+        S_ += delta * delta2;
     }
 
-    WindowStats(std::vector<WindowStats> const & stats) {
-        double k = 0;
-        double M = 0;
-        double S = 0;
-        for(size_t i=0; i<stats.size(); ++i) {
-            WindowStats const & stat = stats[i];
-            double k_a = k;
-            k += stat.k_;
-            double delta = stat.S_ - S;
-            M = M + delta * stat.k_ / k;
-            S += stat.M_ + delta * delta * k_a * stat.k_ / k;
+    void Finalize() {
+        mean = M_;
+        if(k_ > 1)
+            variance = S_ / (k_ - 1);
+        else
+            variance = 0;
+    }
+
+    template<typename T>
+    void AddSamples(std::deque<T> const & buffer) {
+        for(size_t i=0; i<buffer.size(); ++i) {
+            AddSample<T>(buffer[i]);
         }
-        mean = M;
-        variance = S / (k - 1);
-        k_ = k; M_ = M; S_ = M;
+        Finalize();
+    }
+
+    WindowStats() :
+        k_(0), M_(0), S_(0) {
+    }
+
+    template<typename T>
+    WindowStats(std::deque<T> const & buffer) :
+        k_(0), M_(0), S_(0) {
+        AddSamples(buffer);
     }
 };
 
@@ -394,38 +431,52 @@ void CCMBaselineAnalyzer::Process() {
 
     CCMWaveformUInt16Series const & waveforms = frame->Get<CCMWaveformUInt16Series>(waveforms_name_);
 
-    double max_derivative = 0.65;
+    double max_derivative = 0.25;
 
     for(size_t i=0; i<waveforms.size(); ++i) {
         CCMWaveformUInt16 const & wf = waveforms[i];
         WaveformSource<uint16_t> wf_source(wf);
         MultiplicativeFilter<uint16_t, double> m_filter(-1.0, &wf_source);
         BufferView<double, double> buffer_m(&m_filter);
-        ExponentialFilter<double, double> exp_filter(10.0, 2.0, 0.0, &buffer_m);
+        ExponentialFilter<double, double> exp_filter(10.0, 2.0, &buffer_m);
         std::deque<double> smoothing_kernel = {1,1,1};
         SmoothingFilter<double, double> smoothing_filter(1, smoothing_kernel, &exp_filter);
-        DerivativeFilter<double, double> derivative_filter(2.0, &smoothing_filter);
+        BackDerivativeFilter<double, double> derivative_filter(2.0, &smoothing_filter);
         DerivativeWindowCapture<double, double, double> window(derivative_filter, buffer_m, max_derivative);
 
         std::vector<WindowStats> stats;
+        WindowStats total;
 
         try {
             while(true) {
                 bool have_window = window.Next();
                 if(have_window) {
-                    stats.emplace_back(window.GetBuffer());
+                    std::deque<double> window_buffer = window.GetBuffer();
+                    stats.emplace_back(window_buffer);
+                    total.AddSamples(window_buffer);
+                    std::cout << "Window: avg(" << stats.back().mean << ") first(" << window_buffer[0] << ") len(" << window_buffer.size() << ")" << std::endl;
                 }
             }
         } catch(SourceExhausted const & e) {
         }
 
-        WindowStats stat(stats);
-
         /*
          * Do something with this information...
          */
+        double max_length = 0;
+        double tot_length = 0;
+        for(size_t j=0; j<stats.size(); ++j) {
+            max_length = std::max(max_length, stats[j].k_);
+            tot_length += stats[j].k_;
+        }
+        double avg_length = tot_length / stats.size();
+
         std::cout << "N regions found: " << stats.size() << std::endl;
-        std::cout << "Mean ADC: " << stat.mean << std::endl;
+        std::cout << "Max length: " << max_length << std::endl;
+        std::cout << "Avg length: " << avg_length << std::endl;
+        std::cout << "Mean ADC: " << total.mean << std::endl;
+        std::cout << "Std dev: " << sqrt(total.variance) << std::endl;
+        std::cout << std::endl;
     }
 }
 
