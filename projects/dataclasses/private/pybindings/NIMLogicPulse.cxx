d90c359947ef356ec59c96dccd3344946e997b65
Austin Schneider <aschneider@lanl.gov>
2023-05-08 11:14:33 -0600

    Draft NIMLogicPulse

---
+++ projects/dataclasses/private/pybindings/NIMLogicPulse.cxx
@@ -0,0 +1,25 @@
+#include <vector>
+#include <icetray/I3Units.h>
+#include <dataclasses/physics/NIMLogicPulse.h>
+#include <dataclasses/I3Vector.h>
+#include <icetray/python/dataclass_suite.hpp>
+#include <dataclasses/ostream_overloads.hpp>
+
+namespace bp = boost::python;
+
+void register_NIMLogicPulse() {
+    bp::class_<NIMLogicPulse, NIMLogicPulsePtr>("NIMLogicPulse")
+    PROPERTY(NIMLogicPulse, time, NIMPulseTime)
+    PROPERTY(NIMLogicPulse, length, NIMPulseLength)
+    .def( bp::self == bp::self )
+    .def( bp::self != bp::self )
+    .def(bp::dataclass_suite<NIMLogicPulse>())
+    ;
+
+    bp::class_<I3VectorNIMLogicPulse, bp::bases<I3FrameObject>,
+        boost::shared_ptr<I3VectorNIMLogicPulse > >("I3VectorNIMLogicPulse")
+        .def(bp::dataclass_suite<I3VectorNIMLogicPulse >())
+        ;
+
+    register_pointer_conversions<I3VectorNIMLogicPulse>();
+}
