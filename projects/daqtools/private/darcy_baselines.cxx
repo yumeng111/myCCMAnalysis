9ac63a10e761977588e82650d7d68afb7299f24a
Austin Schneider <aschneider@lanl.gov>
2023-07-21 10:02:42 -0600

    Add flat and linear fits to baselines

--- projects/daqtools/private/darcy_baselines.cxx
+++ projects/daqtools/private/darcy_baselines.cxx
@@ -37,13 +37,85 @@
 #include "daqtools/WaveformSmoother.h"
 #include <dataclasses/calibration/BaselineEstimate.h>
 
+void LinearFlatFit(std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end, double & linear_b, double & linear_m, double & linear_sigma, double & flat_mean, double & flat_sigma) {
+    double N = std::distance(begin, end);
+    double X = 0.0;
+    double X2 = 0.0;
+    double T = 0.0;
+    double T2 = 0.0;
+    double XT = 0.0;
+
+    size_t t = 0;
+    std::vector<double>::const_iterator it = begin;
+    for(; it != end; ++it) {
+        double x = *it;
+        X += x;
+        X2 += x*x;
+        T += t;
+        T2 += t*t;
+        XT += t*x;
+
+        ++t;
+    }
+    double b = X/N - (N*XT*T-X*T*T) / (N*N*T2 - N*T*T);
+    double m = (N*XT - X*T) / (N*T2 - T*T);
+    double sigma2 = (X2 + N*b*b + m*m*T2 - 2*b*X - 2*m*XT) / N;
+    linear_b = b;
+    linear_m = m;
+    linear_sigma = sqrt(sigma2);
+    flat_mean = X / N;
+
+    flat_sigma = 0;
+    it = begin;
+    for(; it != end; ++it) {
+        flat_sigma += ((*it) - flat_mean) * ((*it) - flat_mean);
+    }
+    flat_sigma /= N;
+}
+
+double FlatChi2PerDOF(std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end, double flat_mean, double flat_sigma) {
+    size_t N = std::distance(begin, end);
+    size_t DOF = std::max(N, 3) - 2;
+    double stddev = flat_sigma;
+    double pred = flat_mean;
+    double chi2 = 0.0;
+    for(; begin != end; ++begin) {
+        double z = ((*begin) - pred) / stddev;
+        chi2 += z * z;
+    }
+    chi2 /= 2.0;
+    chi2 += log(stddev) + log(sqrt(2.0 * M_PI));
+    return chi2 /= DOF;
+}
+
+double LinearChi2PerDOF(
+        std::vector<double>::const_iterator begin,
+        std::vector<double>::const_iterator end,
+        double linear_intercept,
+        double linear_slope,
+        double linear_sigma) {
+    size_t N = std::distance(begin, end);
+    size_t DOF = std::max(N, 4) - 3;
+    double stddev = linear_sigma;
+    double chi2 = 0.0;
+    size_t t = 0;
+    for(; begin != end; ++begin, ++t) {
+        double pred = linear_intercept + linear_slope * t;
+        double z = ((*begin) - pred) / stddev;
+        chi2 += z * z;
+    }
+    chi2 /= 2.0;
+    chi2 += log(stddev) + log(sqrt(2.0 * M_PI));
+    return chi2 /= DOF;
+}
+
 class darcy_baselines: public I3Module {
     bool geo_seen;
     std::string geometry_name_;
     I3Map<CCMPMTKey, uint32_t> pmt_channel_map_;
     void Geometry(I3FramePtr frame);
     void ProcessWaveform(std::vector<short unsigned int> const & samples, BaselineEstimate & baseline);
-    void FitExponential(std::vector<double> & y, double & a, double & b, double & c);
+    void FitExponential(std::vector<double> const & y, double & a, double & b, double & c);
     void OutlierFilter(std::vector<short unsigned int> const & samples, std::vector<double> & outlier_filter_results);
     public:
     darcy_baselines(const I3Context&);
@@ -83,7 +155,7 @@ void darcy_baselines::OutlierFilter(std::vector<short unsigned int> const & samp
     double starting_val = 0;
     double counter = 0;
 
-    for (size_t it = 0; it < 10; ++it){
+    for(size_t it = 0; it < 10; ++it) {
         starting_val += samples[it];
         counter += 1;
     }
@@ -91,15 +163,15 @@ void darcy_baselines::OutlierFilter(std::vector<short unsigned int> const & samp
     double value = starting_val/counter;
 
     // now let's loop over the waveform
-    for (size_t wf_it = 0; wf_it < samples.size(); ++wf_it){
+    for(size_t wf_it = 0; wf_it < samples.size(); ++wf_it) {
         double delta = samples[wf_it] - value;
         double e = std::fabs(delta) / delta_tau;
 
-        if (wf_it > 0){
+        if(wf_it > 0) {
             e += std::fabs(samples[wf_it] - samples[wf_it - 1]) / prev_tau;
         }
 
-        if (wf_it + 1 < samples.size()){
+        if(wf_it + 1 < samples.size()) {
             e += std::fabs(samples[wf_it] - samples[wf_it + 1]) / next_tau;
         }
         delta *= std::exp(-e);
@@ -109,7 +181,7 @@ void darcy_baselines::OutlierFilter(std::vector<short unsigned int> const & samp
 
 }
 
-void darcy_baselines::FitExponential(std::vector<double> & y, double & a, double & b, double & c){
+void darcy_baselines::FitExponential(std::vector<double> const & y, double & a, double & b, double & c){
 
     std::vector<double> x (y.size());
     // let's fill our x vals (aka time)
@@ -193,7 +265,6 @@ void darcy_baselines::FitExponential(std::vector<double> & y, double & a, double
 
     a = m_inv00 * v0 + m_inv01 * v1;
     b = m_inv01 * v0 + m_inv11 * v1;
-
 }
 
 
@@ -207,13 +278,43 @@ void darcy_baselines::ProcessWaveform(std::vector<short unsigned int> const & sa
     std::vector<double> outlier_filter_results(samples.size());
     OutlierFilter(samples, outlier_filter_results);
 
+    double flat_mean = 0;
+    double flat_sigma = 0;
+    double linear_intercept = 0;
+    double linear_slope = 0;
+    double linear_sigma = 0;
+    LinearFlatFit(outlier_filter_results.begin(), outlier_filter_results.end(), linear_intercept, linear_slope, linear_sigma, flat_mean, flat_sigma);
+
+    double flat_chi2_per_dof_threshold = 1.0;
+    double flat_sigma_threshold = 30.0;
+    double flat_chi2_per_dof = FlatChi2PerDOF(outlier_filter_results.begin(), outlier_filter_results.end(), flat_mean, flat_sigma);
+    if(flat_chi2_per_dof < flat_chi2_per_dof_threshold and flat_sigma < flat_sigma_threshold) {
+        // a flat fit is good enough
+        // Let's use the mode of the outlier filter as the baseline estimate
+    
+        return;
+    }
+
+    double linear_chi2_per_dof_threshold = 1.0;
+    double linear_sigma_threshold = 10.0;
+    double linear_slope_threshold = 1.0;
+    double linear_chi2_per_dof = LinearChi2PerDOF(outlier_filter_results.begin(), outlier_filter_results.end(), linear_intercept, linear_slope, linear_sigma);
+    if(linear_chi2_per_dof < linear_chi2_per_dof_threshold and linear_sigma < linear_sigma_threshold and linear_slope < linear_slope_threshold) {
+        // a linear fit is good enough
+        // Let's subtract off the linear component and use the mode of result as the baseline
+
+        return;
+    }
+
+    // Flat and linear are not good enough, let's try an exponential
+
     // now let's put the result through the exponential fitter
     double a = 0;
     double b = 0;
     double c = 0;
     FitExponential(outlier_filter_results, a, b, c);
 
-    if (a == 0 or b == 0 or c == 0){
+    if (a == 0 or b == 0 or c == 0) {
         // oops! exponential fit didnt go right
         // let's just use the mode of the outlier filter for the baselines
         std::sort(outlier_filter_results.begin(), outlier_filter_results.end());
@@ -225,9 +326,7 @@ void darcy_baselines::ProcessWaveform(std::vector<short unsigned int> const & sa
         baseline.target_num_frames = 0;
         baseline.num_frames = 0;
         baseline.num_samples = 0;
-    }
-
-    else{
+    } else {
         // now let's get the mode of our exponential fit!!
         std::vector<double> exp_result(samples.size());
         double current_time;
