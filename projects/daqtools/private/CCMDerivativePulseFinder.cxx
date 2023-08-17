543b04bde5df78bc33298135d22efb4b9136cbbf
Austin Schneider <aschn@mit.edu>
2023-08-17 10:45:34 -0600

    Sketch derivative pulse finder module

---
+++ projects/daqtools/private/CCMDerivativePulseFinder.cxx
@@ -0,0 +1,237 @@
+#include <icetray/IcetrayFwd.h>
+
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/foreach.hpp>
+#include <boost/make_shared.hpp>
+
+
+#include <set>
+#include <tuple>
+#include <cctype>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <thread>
+#include <cmath>
+#include <functional>
+
+#include <icetray/I3Frame.h>
+#include <icetray/I3TrayInfo.h>
+#include <icetray/I3Module.h>
+#include <icetray/I3Logging.h>
+#include <icetray/I3PODHolder.h>
+#include <icetray/CCMPMTKey.h>
+#include <icetray/CCMTriggerKey.h>
+#include <dataclasses/physics/CCMWaveform.h>
+#include <dataclasses/geometry/CCMGeometry.h>
+#include <dataclasses/physics/CCMRecoPulse.h>
+#include <dataclasses/calibration/CCMPMTCalibration.h>
+#include <dataclasses/calibration/BaselineEstimate.h>
+#include <daqtools/WaveformDerivative.h>
+
+class CCMDerivativePulseFinder: public I3Module {
+    bool geo_seen;
+    std::string geometry_name_;
+    std::string ccm_waveform_name_;
+    bool throw_without_input_;
+    std::string output_name_;
+
+    double pulse_initial_deriv_threshold;
+    size_t pulse_max_width;
+    size_t pulse_min_width;
+    double pulse_min_height;
+    double pulse_min_deriv_magnitude;
+    double pulse_min_integral;
+
+    I3Map<CCMPMTKey, uint32_t> pmt_channel_map_;
+public:
+    CCMDerivativePulseFinder(const I3Context&);
+    void Configure();
+    void DAQ(I3FramePtr frame);
+    void Geometry(I3FramePtr frame);
+    static std::vector<CCMRecoPulse> CheckForPulse(WaveformDerivative & deriv, size_t start_idx, size_t max_samples, size_t min_length, double value_threshold, double derivative_threshold, double integral_threshold);
+    static void FindPulses(
+        CCMRecoPulseSeries & output,
+        WaveformDerivative & deriv,
+        double deriv_threshold,
+        size_t max_pulse_width,
+        size_t min_pulse_width,
+        double min_pulse_height,
+        double min_deriv_magnitude,
+        double min_integral);
+};
+
+I3_MODULE(CCMDerivativePulseFinder);
+
+CCMDerivativePulseFinder::CCMDerivativePulseFinder(const I3Context& context) : I3Module(context), 
+    geometry_name_(""), geo_seen(false) {
+    AddParameter("CCMGeometryName", "Key for CCMGeometry", std::string(I3DefaultName<CCMGeometry>::value()));
+    AddParameter("CCMCalibratedWaveformsName", "Key for the input CCMWaveformDoubleSeries", std::string("CCMCalibratedWaveforms"));
+    AddParameter("ThrowWithoutInput", "Whether to throw an error when there is no input", false);
+
+    AddParameter("InitialDerivativeThreshold", "Initial positive derivative threshold for a pulse", double(0.3));
+    AddParameter("MinPulseWidth", "Minimum width for defining a pulse", size_t(5));
+    AddParameter("MaxPulseWidth", "Maxiumum width for defining a pulse", size_t(100));
+    AddParameter("MinPulseHeight", "Minimum height for defining a pulse", double(5.0));
+    AddParameter("MinPulseDerivativeMagnitude", "Minimum derivative magnitude for defining a pulse", double(0.65));
+    AddParameter("MinPulseIntegral", "Minimum integral for defining a pulse", double(25.0));
+    AddParameter("OutputName", "Key for output CCMRecoPulseSeriesMap", std::string("DerivativePulses"));
+}
+
+void CCMDerivativePulseFinder::Configure() {
+    GetParameter("CCMGeometryName", geometry_name_);
+    GetParameter("CCMCalibratedWaveformsName", ccm_waveform_name_);
+    GetParameter("ThrowWithoutInput", throw_without_input_);
+    GetParameter("InitialDerivativeThreshold", pulse_initial_deriv_threshold);
+    GetParameter("MinPulseWidth", pulse_min_width);
+    GetParameter("MaxPulseWidth", pulse_max_width);
+    GetParameter("MinPulseHeight", pulse_min_height);
+    GetParameter("MinPulseDerivativeMagnitude", pulse_min_deriv_magnitude);
+    GetParameter("MinPulseIntegral", pulse_min_integral);
+    GetParameter("OutputName", output_name_);
+}
+
+
+void CCMDerivativePulseFinder::Geometry(I3FramePtr frame) {
+    if(not frame->Has(geometry_name_)) {
+        log_fatal("Could not find CCMGeometry object with the key named \"%s\" in the Geometry frame.", geometry_name_);
+    }
+    CCMGeometry const & geo = frame->Get<CCMGeometry const>(geometry_name_);
+    pmt_channel_map_ = geo.pmt_channel_map;
+    geo_seen = true;
+    PushFrame(frame);
+}
+
+void CCMDerivativePulseFinder::DAQ(I3FramePtr frame) {
+
+    if(not frame->Has(ccm_waveform_name_)) {
+        if(throw_without_input_) {
+            log_fatal("Input waveform name %s not present", ccm_waveform_name_);
+        } else {
+            log_warn("Input waveform name %s not present", ccm_waveform_name_);
+        }
+        return;
+    }
+
+    // let's read in our waveform
+    boost::shared_ptr<const CCMWaveformDoubleSeries> waveforms = frame->Get<boost::shared_ptr<const CCMWaveformDoubleSeries>>(ccm_waveform_name_);
+
+    std::vector<CCMPMTKey> pmt_keys;
+    pmt_keys.reserve(pmt_channel_map_.size());
+    for(std::pair<CCMPMTKey const, uint32_t> const & p : pmt_channel_map_) {
+        pmt_keys.push_back(p.first);
+    }
+
+    boost::shared_ptr<CCMRecoPulseSeriesMap> output(new CCMRecoPulseSeriesMap);
+
+    // loop over each channel in waveforms
+    for(size_t i=0; i<pmt_keys.size(); ++i) {
+        CCMPMTKey key = pmt_keys[i];
+        uint32_t channel = pmt_channel_map_[key];
+        WaveformDerivative deriv(waveforms->at(channel).GetWaveform().begin(), waveforms->at(channel).GetWaveform().end(), 2.0);
+        FindPulses(output->operator[](key),
+                deriv,
+                pulse_initial_deriv_threshold,
+                pulse_max_width,
+                pulse_min_width,
+                pulse_min_height,
+                pulse_min_deriv_magnitude,
+                pulse_min_integral);
+    }
+
+    frame->Put(output_name_, output);
+    PushFrame(frame);
+}
+
+void CCMDerivativePulseFinder::FindPulses(
+        CCMRecoPulseSeries & output,
+        WaveformDerivative & deriv,
+        double deriv_threshold,
+        size_t max_pulse_width,
+        size_t min_pulse_width,
+        double min_pulse_height,
+        double min_deriv_magnitude,
+        double min_integral) {
+    // Determine the maximum starting sample for a pulse
+    // We need at least 5 samples after the starting pulse
+    size_t N = deriv.Size();
+    // Define the maximum sample to consider for the start of a pulse
+    // A pulse needs at least 5 samples, so subtract off 5 from the max sample we can look at
+    if(N > 5)
+        N -= 5;
+    size_t pulse_first_index = 0;
+    size_t pulse_last_index = 0;
+
+    // Reset the smoother position to the start of the waveform
+    deriv.Reset();
+    for(size_t i=0; i<N; ++i) {
+        // Check the condition for the beginning of a pulse
+        if(deriv.Derivative() > deriv_threshold) {
+            // Get the last index of the found pulse, 0 if no pulse is found
+            std::vector<CCMRecoPulse> pulses = CheckForPulse(deriv, i, max_pulse_width, min_pulse_width, min_pulse_height, min_deriv_magnitude, min_integral);
+            for(size_t j=0; j<pulses.size(); ++j) {
+                output.push_back(pulses[j]);
+                pulse_first_index = pulses[j].GetTime() / 2;
+                pulse_last_index = pulse_first_index + pulses[j].GetWidth() / 2;
+                i = pulse_last_index;
+            }
+            deriv.Reset(pulse_last_index);
+        }
+        deriv.Next();
+    }
+}
+
+std::vector<CCMRecoPulse> CCMDerivativePulseFinder::CheckForPulse(WaveformDerivative & deriv, size_t start_idx, size_t max_samples, size_t min_length, double value_threshold, double derivative_threshold, double integral_threshold) {
+    deriv.Reset(start_idx);
+    // 0 before start of pulse checking [checking for positive derivative]
+    // 1 derivative is positive (in rising edge of pulse) [checking for negative derivative]
+    // 2 derivative is negative (in falling edge of pulse) [checking for positive derivative]
+    // 3 derivative is positive (recovering from droop) [done]
+    int state = 1;
+    double max_value = deriv.Value();
+    double max_abs_derivative = deriv.Derivative();
+    double integral = 0;
+    bool found_pulse = false;
+    size_t final_idx = start_idx;
+    size_t N = std::min(deriv.Size(), start_idx + max_samples);
+    for(size_t i=start_idx; i<N; ++i) {
+        max_value = std::max(max_value, deriv.Value());
+        max_abs_derivative = std::max(max_abs_derivative, std::abs(deriv.Derivative()));
+        integral += deriv.Value();
+        if(state % 2) {
+            if(deriv.Derivative() < 0) {
+                state += 1;
+            }
+        } else {
+            if(deriv.Derivative() > 0) {
+                state += 1;
+            }
+        }
+        if(state >= 3) {
+            found_pulse = true;
+            final_idx = i;
+            break;
+        }
+        deriv.Next();
+    }
+    if(found_pulse
+            and (max_value >= value_threshold)
+            and (max_abs_derivative >= derivative_threshold)
+            and (integral >= integral_threshold)
+            and (final_idx - start_idx >= min_length)
+            ) {
+        CCMRecoPulse pulse;
+        pulse.SetTime(double(start_idx) * 2.0);
+        pulse.SetCharge(integral);
+        pulse.SetWidth((double(final_idx) - double(start_idx)) * 2.0);
+        return {pulse};
+    } else {
+        deriv.Reset(start_idx);
+        return {};
+    }
+}
---
+++ projects/daqtools/public/daqtools/WaveformDerivative.h
@@ -0,0 +1,104 @@
+#ifndef I3_WaveformDerivative_H
+#define I3_WaveformDerivative_H
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <icetray/I3DefaultName.h>
+#include <icetray/I3FrameObject.h>
+#include <icetray/I3PointerTypedefs.h>
+#include <icetray/serialization.h>
+#include <icetray/I3Logging.h>
+
+class WaveformDerivative : public I3FrameObject {
+    std::vector<double>::const_iterator begin;
+    std::vector<double>::const_iterator end;
+    size_t N;
+    std::vector<double> derivative;
+    size_t index;
+    size_t max_computed;
+    double delta_t;
+    double current_value;
+    double current_derivative;
+    double x;
+public:
+    WaveformDerivative(std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end, double delta_t) :
+        begin(begin), end(end), N(std::distance(begin, end)), derivative(1), index(0), max_computed(0), delta_t(delta_t) {
+
+        // Derivative for the first element
+        derivative[0] = (-3.0 * begin[0] + 4.0 * begin[1] - begin[2]) / (2.0 * delta_t);
+
+        index = 0;
+
+        current_value = begin[index];
+        current_derivative = derivative[index];
+    }
+
+    void Reset() {
+        index = 0;
+    }
+
+    void Reset(size_t reset_index) {
+        if(reset_index > max_computed) {
+            Reset(max_computed);
+            while(max_computed < reset_index) {
+                Next();
+                if(max_computed == N-1)
+                    break;
+            }
+        }
+        index = std::min(reset_index, max_computed);
+    }
+
+    std::pair<std::vector<double>::const_iterator, std::vector<double>::const_iterator> GetRawWaveform() const {
+        return {begin, begin + index+1};
+    }
+
+    std::pair<std::vector<double>::const_iterator, std::vector<double>::const_iterator> GetDerivative() const {
+        return {derivative.cbegin(), derivative.cbegin() + index+1};
+    }
+
+    std::pair<std::vector<double>::const_iterator, std::vector<double>::const_iterator> GetFullDerivative() const {
+        return {derivative.cbegin(), derivative.cend()};
+    }
+
+    int CurrentIndex() const {
+        return index;
+    }
+
+    double Value() const {
+        return begin[index];
+    }
+
+    double Derivative() const {
+        return derivative[index];
+    }
+
+    void Next() {
+        if(index < N-1)
+            index += 1;
+        if(index <= max_computed)
+            return;
+        size_t idx = index;
+        if(idx < N-1) {
+            // Finite difference derivative
+            derivative.push_back((begin[idx+1] - begin[idx-1]) / (2.0 * delta_t));
+        } else if(idx == N-1) {
+            derivative.resize(N);
+
+            // Derivative for the last element
+            derivative[N-1] = (3.0 * begin[N-1] - 4.0 * begin[N-2] + begin[N-3]) / (2.0 * delta_t);
+        }
+        current_value = begin[idx];
+        current_derivative = derivative[idx];
+        max_computed = idx;
+    }
+
+    size_t Size() {
+        return N;
+    }
+};
+
+I3_POINTER_TYPEDEFS(WaveformDerivative);
+
+#endif // I3_WaveformDerivative_H
