4c1d2fb04cf75dbdff077c193c646456cf480eb2
Austin Schneider <aschneider@lanl.gov>
2023-12-14 12:06:19 -0700

    Rename AccumulateWaveforms module to the more specific name: AccumulateSummedWaveforms

--- projects/daqtools/private/daqtools/AccumulateSummedWaveforms.cxx
+++ projects/daqtools/private/daqtools/AccumulateSummedWaveforms.cxx
@@ -27,13 +27,11 @@
 #include "daqtools/WaveformSmoother.h"
 #include "daqtools/WaveformAccumulator.h"
 
-class AccumulateWaveforms : public I3Module {
+class AccumulateSummedWaveforms : public I3Module {
     // Names for keys in the frame
     std::string geometry_name_;
     std::string input_prefix_;
     std::string output_prefix_;
-    double min_counts_;
-    double max_counts_;
     bool consume_frames_;
 
     // Internal state
@@ -41,7 +39,7 @@ class AccumulateWaveforms : public I3Module {
     WaveformAccumulator summed_waveform_;
 
 public:
-    AccumulateWaveforms(const I3Context&);
+    AccumulateSummedWaveforms(const I3Context&);
     void Configure();
     void Geometry(I3FramePtr frame);
     void DAQ(I3FramePtr frame);
@@ -50,48 +48,44 @@ public:
     void ProcessFrame(I3FramePtr frame);
 };
 
-I3_MODULE(AccumulateWaveforms);
+I3_MODULE(AccumulateSummedWaveforms);
 
-void AccumulateWaveforms::ProcessFrame(I3FramePtr frame) {
-    std::vector<double> const & samples = frame->Get<I3Vector<double> const>(input_prefix_ + "SummedWaveform");
-    std::vector<unsigned int> const & counts = frame->Get<I3Vector<unsigned int> const>(input_prefix_ + "SummedWaveformCounts");
-    I3PODHolder<int> const & fixed_position = frame->Get<I3PODHolder<int> const>(input_prefix_ + "SummedWaveformFixedPosition");
+void AccumulateSummedWaveforms::ProcessFrame(I3FramePtr frame) {
+    std::vector<double> const & samples = frame->Get<I3Vector<double> const>(input_prefix_);
+    std::vector<unsigned int> const & counts = frame->Get<I3Vector<unsigned int> const>(input_prefix_ + "Counts");
+    I3PODHolder<int> const & fixed_position = frame->Get<I3PODHolder<int> const>(input_prefix_ + "FixedPosition");
     summed_waveform_.AddWaveform(samples, fixed_position.value, counts);
 }
 
-AccumulateWaveforms::AccumulateWaveforms(const I3Context& context) : I3Module(context),
+AccumulateSummedWaveforms::AccumulateSummedWaveforms(const I3Context& context) : I3Module(context),
     geometry_name_("") {
     AddParameter("CCMGeometryName", "Key for CCMGeometry", std::string(I3DefaultName<CCMGeometry>::value()));
-    AddParameter("InputPrefix", "Prefix for the output of SumWaveforms", std::string(""));
-    AddParameter("OutputPrefix", "Prefix for the module output", std::string(""));
-    AddParameter("MinCountsThreshold", "Minimum ADC counts to accept for pulses", double(20));
-    AddParameter("MaxCountsThreshold", "Maximum ADC counts to accept for pulses", double(50));
+    AddParameter("InputPrefix", "Prefix for the output of SumWaveforms", std::string("SummedWaveform"));
+    AddParameter("OutputPrefix", "Prefix for the module output", std::string("AccumulatedWaveform"));
     AddParameter("ConsumeFrames", "Consume frames used as input?", bool(true));
 }
 
-void AccumulateWaveforms::Configure() {
-    GetParameter("MinCountsThreshold", min_counts_);
-    GetParameter("MaxCountsThreshold", max_counts_);
+void AccumulateSummedWaveforms::Configure() {
     GetParameter("CCMGeometryName", geometry_name_);
     GetParameter("InputPrefix", input_prefix_);
     GetParameter("OutputPrefix", output_prefix_);
     GetParameter("ConsumeFrames", consume_frames_);
 }
 
-void AccumulateWaveforms::Geometry(I3FramePtr frame) {
+void AccumulateSummedWaveforms::Geometry(I3FramePtr frame) {
     if(not frame->Has(geometry_name_)) {
         log_fatal("Could not find CCMGeometry object with the key named \"%s\" in the Geometry frame.", geometry_name_);
     }
     PushFrame(frame);
 }
 
-void AccumulateWaveforms::DAQ(I3FramePtr frame) {
+void AccumulateSummedWaveforms::DAQ(I3FramePtr frame) {
     ProcessFrame(frame);
     if(not consume_frames_)
         PushFrame(frame);
 }
 
-void AccumulateWaveforms::Finish() {
+void AccumulateSummedWaveforms::Finish() {
 
     boost::shared_ptr<I3PODHolder<int32_t>> p_fixed_position = boost::make_shared<I3PODHolder<int32_t>>();
 
@@ -106,9 +100,9 @@ void AccumulateWaveforms::Finish() {
 
     I3FramePtr frame = boost::make_shared<I3Frame>(I3Frame::DAQ);
 
-    frame->Put((output_prefix_ + "AccumulatedWaveform").c_str(), p_samples);
-    frame->Put((output_prefix_ + "AccumulatedWaveformCounts").c_str(), p_counts);
-    frame->Put((output_prefix_ + "AccumulatedWaveformFixedPosition").c_str(), p_fixed_position);
+    frame->Put((output_prefix_).c_str(), p_samples);
+    frame->Put((output_prefix_ + "Counts").c_str(), p_counts);
+    frame->Put((output_prefix_ + "FixedPosition").c_str(), p_fixed_position);
 
     PushFrame(frame);
     Flush();
