3cbc0e28a3354eb9dfeed87cfd4686ac1facc907
Austin Schneider <hogenshpogen@gmail.com>
2024-02-16 13:52:34 -0700

    Whitespace

--- projects/simclasses/private/simclasses/CCMMCPE.cxx
+++ projects/simclasses/private/simclasses/CCMMCPE.cxx
@@ -4,15 +4,15 @@
 I3_SERIALIZABLE(CCMMCPESeriesMap);
 
 std::ostream& operator<<(std::ostream& os, const CCMMCPE& pe) {
-  return(pe.Print(os));
+    return(pe.Print(os));
 }
 
 std::ostream& CCMMCPE::Print(std::ostream& os) const{
-  os << "[ CCMMCPE::"
-     << "\n  Time :" << time
-     << "\n  NPE  :" << npe
-     << "\n  " << ID
-     << " ]";
-  return os;
+    os << "[ CCMMCPE::"
+        << "\n  Time :" << time
+        << "\n  NPE  :" << npe
+        << "\n  " << ID
+        << " ]";
+    return os;
 }
 
