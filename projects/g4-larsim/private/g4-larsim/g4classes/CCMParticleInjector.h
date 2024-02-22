64c2e6e566200c49e66031692a46f9e4a2471ab7
Darcy Angelie Newmark <dnewmark@mit.edu>
2024-02-22 15:47:24 -0500

    updated CCMParticleInjector.h to inherit from I3ServiceBase

--- projects/g4-larsim/private/g4-larsim/g4classes/CCMParticleInjector.h
+++ projects/g4-larsim/private/g4-larsim/g4classes/CCMParticleInjector.h
@@ -1,17 +1,16 @@
 #ifndef CCMPARTICLEINJECTOR_H
-#define CCMPARTICLEINJECTOR_H 1
+#define CCMPARTICLEINJECTOR_H
 
+#include <dataclasses/physics/I3MCTree.h>
+#include <dataclasses/physics/I3MCTreeUtils.h>
+#include <dataclasses/physics/I3Particle.h>
 #include <icetray/I3Context.h>
 #include <icetray/I3Configuration.h>
+#include <icetray/I3ServiceBase.h>
 #include <icetray/I3PointerTypedefs.h>
-#include <dataclasses/geometry/I3Geometry.h>
-#include <dataclasses/calibration/I3Calibration.h>
-#include <dataclasses/status/I3DetectorStatus.h>
-#include <g4-larsim/g4classes/ExtendedI3Particle.h>
-#include <map>
-#include <boost/variant.hpp>
+#include <icetray/I3SingleServiceFactory.h>
 
-class CCMParticleInjector
+class CCMParticleInjector : public I3ServiceBase
 {
  public:
 
@@ -23,82 +22,11 @@ class CCMParticleInjector
   virtual ~CCMParticleInjector() {}
 
   virtual void Configure() {}
-
-  virtual void Initialize(const I3Geometry& geometry,
-                          const I3Calibration& calib,
-                          const I3DetectorStatus& status){}
-
-  /**
-   * X coordinate of detector center
-   */
-  virtual double GetX() const = 0;
-
-  /**
-   * Y coordinate of detector center
-   */
-  virtual double GetY() const = 0;
-
-  /**
-   * Z coordinate of detector center
-   */
-  virtual double GetZ() const = 0;
-
-  /*
-   * detector height
-   */
-  virtual double GetDetectorHeight() const = 0;
-  
-  /*
-   * detector radius
-   */
-  virtual double GetDetectorRadius() const = 0;
-
-
-  /**
-   * This method is called by the I3Topsimulator to do the tracking of a particle
-   * It should return True if the particle has hit a pmt (it generated a signal).
-   */
-  virtual bool TrackParticle(const ExtendedI3Particle& particle,
-                             HitHistoCollection& hitHC,
-                             HitHistoCollection& cherHitCollectionHit,
-                             HitHistoCollection& scintHitCollection) { return false; }
-
-  /**
-   * This method is called by the I3Topsimulator at the beginning of an event
-   */
-  virtual void BeginEvent(const I3Particle& primary) {}
-
-  /**
-   * This method is called by the I3Topsimulator at the end of an event
-   */
-  virtual void EndEvent(HitHistoCollection &hitHC,
-                        HitHistoCollection& cherHitCollection,
-                        HitHistoCollection& scintHitCollection) {}
-
-  const I3Configuration& GetConfiguration() { return configuration_; }
-
-protected:
-
-  template <class ParamType>
-    void AddParameter(const std::string& name,
-                      const std::string& description,
-                      const ParamType& defaultValue)
-  {
-    if (!configuration_.Has(name))
-      configuration_.Add(name, description, defaultValue);
-  }
-
-  template <class ParamType>
-  void GetParameter(const std::string& name, ParamType& value)
-  {
-    value = configuration_.Get<ParamType>(name);
-  }
-
-  const I3Context& GetContext()
-  { return context_; }
-
+  virtual I3MCTreePtr GetI3MCTree() {}
+  virtual I3FramePtr GetI3ConfigurationFrame() {}
 
  private:
+
   const I3Context& context_;
   I3Configuration& configuration_;
 
