e9723305c5c2f9e9353b82403bb16cf0d232a524
Austin Schneider <hogenshpogen@gmail.com>
2024-05-03 14:28:09 -0600

    Sketch light profile code

--- projects/analytic-light-yields/private/analytic-light-yields/LArScintillationLightProfile.cxx
+++ projects/analytic-light-yields/private/analytic-light-yields/LArScintillationLightProfile.cxx
@@ -6,6 +6,7 @@
 #include <boost/foreach.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/math/special_functions.hpp>
+#include <boost/python.hpp>
 
 #include <set>
 #include <tuple>
@@ -29,11 +30,23 @@
 #include <icetray/I3Module.h>
 #include <analytic-light-yields/LArScintillationLightProfile.h>
 
-double gamma_m1(double const & x){
-
-    double E1 = -boost::math::expint(-x);
-    return std::pow(x, -1.0) * std::exp(-x) - E1;
-
+double mpmath_gammainc(double const & a, double const & x) {
+    namespace bp = boost::python;
+    bp::object builtins = bp::import("builtins");
+    bp::object mpmath_embed;
+    try {
+        mpmath_embed = bp::import("mpmath");
+    catch( bp::error_already_set& e ) {
+        log_error("Unable to import mpmath");
+        PyErr_Clear();
+        return;
+    }
+    bp::object gammainc_function = mpmath_embed.attr("gammainc");
+    bp::object py_result = gammainc_function(a, x);
+    bp::object py_real_result = py_result.attr("real");
+    bp::object py_float_result = builtins.attr("float")(py_real_result);
+    double result = bp::extract<double>(py_float_result);
+    return result;
 }
 
 void get_total_light_profile(double const & R_s,
@@ -43,42 +56,32 @@ void get_total_light_profile(double const & R_s,
                             double const & tau_rec,
                             double const & tau_TPB,
                             std::vector<double> const & times,
-                            std::vector<double> & final_light_profile){
+                            std::vector<double> & final_light_profile) {
 
     // times is a vector of times to calculate the light profile for
     double fraction_recombination = (1.0 - R_s - R_t);
-    double exp_singlet;
-    double exp_triplet;
-    double exp_recombination;
-    double exp_prompt_TPB;
-    double gamma_const;
-    double gamma_t;
-    double log_t;
-    double one;
-    double two;
-    double three;
-    double t;
+    double gamma_const = mpmath_gammainc(-1, -tau_rec / tau_TPB);
+    double coeff_one = Rs / (tau_s - tau_TPB);
+    double coeff_two = Rt / (tau_t - tau_TPB);
+    double coeff_three = fraction_recombination * tau_rec / (tau_TPB * tau_TPB);
 
     // let's loop over times and calculate the light profile at each time
-    for (size_t time_it = 0; time_it < times.size(); time_it++){
+    for (size_t time_it = 0; time_it < times.size(); time_it++) {
+        double const & t = times.at(time_it);
+        double exp_singlet = std::exp(-t / tau_s);
+        double exp_triplet = std::exp(-t / tau_t);
+        double exp_recombination = std::exp(-(t+tau_rec) / tau_TPB);
+        double exp_prompt_TPB = std::exp(-t / tau_TPB);
+        double gamma_t = mpmath_gammainc(-1, -(t + tau_rec) / tau_TPB);
 
-        t = times.at(time_it);
-        exp_singlet = std::exp(-t / tau_s);
-        exp_triplet = std::exp(-t / tau_t);
-        exp_recombination = std::exp(-(t+tau_rec) / tau_TPB);
-        exp_prompt_TPB = std::exp(-t / tau_TPB);
+        double one = coeff_one * (exp_singlet - exp_prompt_TPB);
+        double two = coeff_two * (exp_triplet - exp_prompt_TPB);
+        double three = coeff_three * exp_recombination * (gamma_t - gamma_const);
 
-        gamma_const = gamma_m1(tau_rec / tau_TPB);
-        gamma_t = gamma_m1((t + tau_rec)/tau_TPB);
-        log_t = 2.0 * std::log(t / tau_rec + 1.0);
+        double y = one + two + three;
 
-        one = R_s * (exp_singlet - exp_prompt_TPB) / (tau_s - tau_TPB);
-        two = R_t * (exp_triplet - exp_prompt_TPB) / (tau_t - tau_TPB);
-        three = fraction_recombination * exp_recombination * tau_rec * (gamma_t - gamma_const + log_t) / (tau_TPB * tau_TPB);
-
-        final_light_profile.at(time_it) = one + two + three;
+        final_light_profile.at(time_it) = y;
     }
-
 }
 
 
