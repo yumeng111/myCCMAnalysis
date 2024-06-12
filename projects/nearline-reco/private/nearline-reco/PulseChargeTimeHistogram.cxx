32d527f8fdcab679fd9d2739d02a710ffaa58e14
Austin Schneider <hogenshpogen@gmail.com>
2024-06-12 04:58:20 -0500

    Charge time histogram

---
+++ projects/nearline-reco/private/nearline-reco/PulseChargeTimeHistogram.cxx
@@ -0,0 +1,274 @@
+#include <icetray/IcetrayFwd.h>
+
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/foreach.hpp>
+#include <boost/make_shared.hpp>
+
+#include <set>
+#include <tuple>
+#include <cctype>
+#include <limits>
+#include <string>
+#include <fstream>
+#include <numeric>
+#include <iostream>
+
+#include <boost/histogram.hpp>
+
+#include <icetray/open.h>
+#include <icetray/I3Frame.h>
+#include <icetray/I3TrayInfo.h>
+#include <icetray/I3Module.h>
+#include <icetray/I3Logging.h>
+#include <icetray/I3PODHolder.h>
+#include <icetray/CCMPMTKey.h>
+#include <icetray/CCMTriggerKey.h>
+#include <icetray/robust_statistics.h>
+#include <icetray/I3Int.h>
+#include <dataclasses/I3Double.h>
+#include <dataclasses/I3String.h>
+#include <dataclasses/I3MapCCMPMTKeyMask.h>
+#include <dataclasses/physics/CCMWaveform.h>
+#include <dataclasses/geometry/CCMGeometry.h>
+#include <dataclasses/calibration/CCMPMTCalibration.h>
+#include <dataclasses/calibration/I3DOMCalibration.h>
+#include <dataclasses/physics/CCMWaveform.h>
+#include <dataclasses/physics/CCMBCMSummary.h>
+#include <dataclasses/physics/CCMRecoPulse.h>
+#include <dataclasses/geometry/CCMGeometry.h>
+
+typedef std::tuple<CCMPMTKey, CCMRecoPulse> PMTKeyPulsePair;
+typedef std::vector<PMTKeyPulsePair> PMTKeyPulseVector;
+
+class PulseChargeTimeHistogram: public I3Module {
+    bool geo_seen;
+    std::string geometry_name_;
+    CCMGeometryConstPtr geo;
+    std::string pulses_name_;
+
+    std::string output_prefix_;
+
+    double time_low_;
+    double time_high_;
+    size_t time_n_edges_;
+
+    double charge_log_low_;
+    double charge_log_high_;
+    size_t charge_n_edges_;
+    double charge_base_;
+
+    bool output_per_pmt_;
+
+    using storage_t = boost::histogram::dense_storage<boost::histogram::accumulators::weighted_sum<double>>;
+    using time_axis_t =
+        boost::histogram::axis::regular<double>;
+    using charge_axis_t =
+        boost::histogram::axis::regular<double, boost::histogram::axis::transform::log>;
+
+    storage_t storage_;
+    time_axis_t time_axis_;
+    charge_axis_t charge_axis_;
+
+    using axes_t = std::tuple<
+        time_axis_t,
+        charge_axis_t
+    >;
+    using hist_t = boost::histogram::histogram<axes_t, storage_t>;
+
+    using time_axes_t = std::tuple<
+        time_axis_t
+    >;
+    using charge_axes_t = std::tuple<
+        charge_axis_t
+    >;
+    using time_hist_t = boost::histogram::histogram<time_axes_t, storage_t>;
+    using charge_hist_t = boost::histogram::histogram<charge_axes_t, storage_t>;
+
+
+    I3Map<CCMPMTKey, hist_t> hists_;
+    I3Map<CCMPMTKey, time_hist_t> time_hists_;
+    I3Map<CCMPMTKey, charge_hist_t> charge_hists_;
+    I3Map<CCMPMTKey, time_hist_t> time_chargeW_hists_;
+
+public:
+    void Geometry(I3FramePtr frame);
+    PulseChargeTimeHistogram(const I3Context&);
+    void Configure();
+    void DAQ(I3FramePtr frame);
+    void Finish();
+};
+
+I3_MODULE(PulseChargeTimeHistogram);
+
+PulseChargeTimeHistogram::PulseChargeTimeHistogram(const I3Context& context) : I3Module(context),
+    geo_seen(false), geometry_name_("") {
+        AddParameter("CCMGeometryName", "Key for CCMGeometry", std::string(I3DefaultName<CCMGeometry>::value()));
+        AddParameter("InputPulsesName", "Name of the input pulses", std::string("WavedeformPulses"));
+        AddParameter("OutputPrefix", "Prefix for the outputs", std::string(""));
+        AddParameter("TimeLow", "Low edge of the log binning", double(-16000));
+        AddParameter("TimeHigh", "High edge of the log binning", double(16000));
+        AddParameter("TimeNEdges", "Number of edges in the log binning", size_t(32000));
+        AddParameter("ChargeLogLow", "Low edge of the log binning", double(-2.0));
+        AddParameter("ChargeLogHigh", "High edge of the log binning", double(9.0));
+        AddParameter("ChargeNEdges", "Number of edges in the log binning", size_t(10*11+1));
+        AddParameter("ChargeBase", "Base of the log binning", double(10.0));
+        AddParameter("OutputPerPMT", "Output one file per PMT", bool(false));
+}
+
+void PulseChargeTimeHistogram::Configure() {
+    GetParameter("CCMGeometryName", geometry_name_);
+    GetParameter("InputPulsesName", pulses_name_);
+    GetParameter("OutputPrefix", output_prefix_);
+    GetParameter("TimeLow", time_low_);
+    GetParameter("TimeHigh", time_high_);
+    GetParameter("TimeNEdges", time_n_edges_);
+    GetParameter("ChargeLogLow", charge_log_low_);
+    GetParameter("ChargeLogHigh", charge_log_high_);
+    GetParameter("ChargeNEdges", charge_n_edges_);
+    GetParameter("ChargeBase", charge_base_);
+    GetParameter("OutputPerPMT", output_per_pmt_);
+
+    time_axis_ = time_axis_t(time_n_edges_, time_low_, time_high_);
+    charge_axis_ = charge_axis_t(charge_n_edges_, std::pow(charge_log_low_, charge_base_), std::pow(charge_log_high_, charge_base_));
+}
+
+void PulseChargeTimeHistogram::Geometry(I3FramePtr frame) {
+    if(not frame->Has(geometry_name_)) {
+        log_fatal("Could not find CCMGeometry object with the key named \"%s\" in the Geometry frame.", geometry_name_.c_str());
+    }
+    geo = frame->Get<CCMGeometryConstPtr>(geometry_name_);
+    geo_seen = bool(geo);
+    if(geo_seen) {
+        for(std::pair<CCMPMTKey const, CCMOMGeo> const & p : geo->pmt_geo) {
+            hists_.insert(
+                    std::make_pair(
+                        p.first,
+                        boost::histogram::make_histogram_with(
+                            storage_,
+                            time_axis_,
+                            charge_axis_
+                            )
+                        )
+                    );
+            time_hists_.insert(
+                    std::make_pair(
+                        p.first,
+                        boost::histogram::make_histogram_with(
+                            storage_,
+                            time_axis_
+                            )
+                        )
+                    );
+            charge_hists_.insert(
+                    std::make_pair(
+                        p.first,
+                        boost::histogram::make_histogram_with(
+                            storage_,
+                            charge_axis_
+                            )
+                        )
+                    );
+            time_chargeW_hists_.insert(
+                    std::make_pair(
+                        p.first,
+                        boost::histogram::make_histogram_with(
+                            storage_,
+                            time_axis_
+                            )
+                        )
+                    );
+        }
+    }
+    PushFrame(frame);
+}
+
+void PulseChargeTimeHistogram::DAQ(I3FramePtr frame) {
+    if(not geo_seen) {
+        log_fatal("No Geometry frame seen yet.");
+    }
+
+    CCMRecoPulseSeriesMapConstPtr pulses = frame->Get<CCMRecoPulseSeriesMapConstPtr>(pulses_name_);
+    if(not pulses) {
+        log_fatal("Could not find %s in the DAQ frame.", pulses_name_.c_str());
+    }
+
+    for (CCMRecoPulseSeriesMap::const_iterator i = pulses->begin(); i != pulses->end(); i++) {
+        hist_t & hist = hists_.at(i->first);
+        time_hist_t & time_hist = time_hists_.at(i->first);
+        charge_hist_t & charge_hist = charge_hists_.at(i->first);
+        time_hist_t & time_chargeW_hist = time_chargeW_hists_.at(i->first);
+        for(CCMRecoPulse const & pulse: i->second) {
+            hist(std::make_tuple(pulse.GetTime(), pulse.GetCharge()), boost::histogram::weight(1.0));
+            time_hist(pulse.GetTime(), boost::histogram::weight(1.0));
+            charge_hist(pulse.GetCharge(), boost::histogram::weight(1.0));
+            time_chargeW_hist(pulse.GetTime(), boost::histogram::weight(pulse.GetCharge()));
+        }
+    }
+
+    PushFrame(frame);
+}
+
+void PulseChargeTimeHistogram::Finish() {
+    /*
+    std::string output_prefix = output_prefix_;
+    if(output_prefix.empty()) {
+        output_prefix = pulses_name_;
+    }
+
+    std::vector<double> bin_edges = np_logspace(log_low_, log_high_, n_edges_, base_);
+
+    std::stringstream ss;
+    ss << output_prefix << ".txt";
+    std::string output_name = ss.str();
+    std::ofstream output_file(output_name.c_str());
+    if(not output_file) {
+        log_fatal("Could not open file \"%s\" for writing.", output_name.c_str());
+    }
+    output_file << bin_edges.at(0);
+    for(size_t bin_idx=1; bin_idx<n_edges_; ++bin_idx) {
+        output_file << " " << bin_edges.at(bin_idx);
+    }
+    output_file << std::endl;
+
+    std::vector<double> total_charge_hist(n_edges_, 0.0);
+    for(std::pair<CCMPMTKey const, std::vector<double>> const & p : *charge_hists_) {
+        for(size_t bin_idx=0; bin_idx<n_edges_; ++bin_idx) {
+            total_charge_hist.at(bin_idx) += p.second.at(bin_idx);
+        }
+    }
+    output_file << total_charge_hist.at(0);
+    for(size_t bin_idx=1; (bin_idx + 1) <n_edges_; ++bin_idx) {
+        output_file << " " << total_charge_hist.at(bin_idx);
+    }
+    output_file << std::endl;
+
+    if(not output_per_pmt_)
+        return;
+
+    for(std::pair<CCMPMTKey const, std::vector<double>> const & p : *charge_hists_) {
+        std::stringstream ss;
+        ss << output_prefix << "_pmt_" << p.first.GetRegion() << "_" << p.first.GetSensor();
+        ss << "_" << static_cast<uint64_t>(p.first.GetSubsensor()) << ".txt";
+        std::string output_name = ss.str();
+        std::ofstream output_file(output_name.c_str());
+        if(not output_file) {
+            log_fatal("Could not open file \"%s\" for writing.", output_name.c_str());
+        }
+        std::vector<double> const & charge_hist = p.second;
+        output_file << bin_edges.at(0);
+        for(size_t bin_idx=1; bin_idx<n_edges_; ++bin_idx) {
+            output_file << " " << bin_edges.at(bin_idx);
+        }
+        output_file << std::endl;
+
+        output_file << charge_hist.at(0);
+        for(size_t bin_idx=1; (bin_idx + 1) <n_edges_; ++bin_idx) {
+            output_file << " " << charge_hist.at(bin_idx);
+        }
+        output_file << std::endl;
+    }
+    */
+}
