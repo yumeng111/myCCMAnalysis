f4063fc4f6c6065612238ae1a89b43e7c62cda19
Austin Schneider <hogenshpogen@gmail.com>
2024-07-07 20:50:03 -0600

    Add common modules

--- projects/daqtools/python/__init__.py
+++ projects/daqtools/python/__init__.py
@@ -3,5 +3,13 @@ from icecube.load_pybindings import load_pybindings
 #load_pybindings(__name__, __path__)
 
 from . import GeometryReplacer
+from . import AddViews
+from . import TriggerTypeFilter
+
+GeometryReplacer = GeometryReplacer.GeometryReplacer
+GeometryReplacer.GeometryReplacer = GeometryReplacer
+
+AddViews = AddViews.AddViews
+TriggerTypeFilter = TriggerTypeFilter.TriggerTypeFilter
 
 del icetray
