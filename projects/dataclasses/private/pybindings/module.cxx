21e4888a1dcaec97192b1cc692cfcc0a9acc429f
Darcy Newmark <dnewmark@mit.edu>
2024-07-13 13:27:37 -0600

    adding I3Map between int and size_t

--- projects/dataclasses/private/pybindings/module.cxx
+++ projects/dataclasses/private/pybindings/module.cxx
@@ -67,7 +67,8 @@ namespace bp = boost::python;
   (MapSubdetectorI3TriggerReadoutConfig)(MapTriggerKeyI3TriggerStatus)  \
   (I3DOMFunctions)(I3MapUnsignedUnsigned)(I3MapUShortUShort)            \
   (I3MapUIntInt)(I3MapUIntVectorUInt)(I3MapUIntVectorDouble)            \
-  (I3MapIntVectorInt)(I3MapKeyVectorDouble)(I3MapKeyDouble)             \
+  (I3MapIntVectorInt)(I3MapIntSizeT)(I3MapKeyVectorDouble)              \
+  (I3MapKeyDouble)                                                      \
   (I3MapKeyUInt)(I3MapKeyVectorInt)(I3MapTriggerDouble)                 \
   (I3MapPMTKeyVectorVectorDouble)(I3MapPMTKeyVectorDouble)              \
   (I3MapPMTKeyDouble)                                                   \
