1f06f57d1ca0f2484c75aaaa079e41200f361cc2
Austin Schneider <hogenshpogen@gmail.com>
2024-08-06 23:21:55 -0600

    Process one DAQ frame at a time. Push an S-frame when necessary. Assume multiple particles in the same input tree belong to the same event (adjust times and ids accordingly)

--- projects/g4-larsim/private/g4-larsim/CCMSimulator.cxx
+++ projects/g4-larsim/private/g4-larsim/CCMSimulator.cxx
@@ -26,7 +26,7 @@ CCMSimulator::CCMSimulator(const I3Context& context): I3Module(context) {
     responseServiceName_ = "CCM200Response";
     AddParameter("ResponseServiceName", "Name of the detector response service.", responseServiceName_);
 
-    injectorServiceName_ = "CCMSimpleInjector"; 
+    injectorServiceName_ = "CCMSimpleInjector";
     AddParameter("InjectorServiceName", "Name of the injector service.", injectorServiceName_);
 
     mcPrimaryName_ = "MCPrimary";
@@ -34,10 +34,10 @@ CCMSimulator::CCMSimulator(const I3Context& context): I3Module(context) {
 
     PMTHitSeriesName_ = "PMTMCHitsMap";
     AddParameter("PMTHitSeriesName", "Name of the resulting PMT hit map in the frame.", PMTHitSeriesName_);
-    
+
     LArMCTreeName_ = "LArMCTree";
     AddParameter("LArMCTreeName", "Name of the MC tree containing energy depositions in LAr", LArMCTreeName_);
-    
+
     PhotonSummarySeriesName_ = "PhotonSummarySeries";
     AddParameter("PhotonSummarySeriesName", "Name of the photon summary series containing optical photon hits in LAr", PhotonSummarySeriesName_);
 
@@ -47,7 +47,7 @@ CCMSimulator::CCMSimulator(const I3Context& context): I3Module(context) {
 
 CCMSimulator::~CCMSimulator() { }
 
- 
+
 void CCMSimulator::Configure() {
     log_info("Configuring the CCMSimulator:");
 
@@ -66,16 +66,16 @@ void CCMSimulator::Configure() {
 
     GetParameter("PMTHitSeriesName", PMTHitSeriesName_);
     log_info("+ PMT hit series : %s", PMTHitSeriesName_.c_str());
-    
+
     GetParameter("LArMCTreeName", LArMCTreeName_);
     log_info("+ LAr MC Tree : %s", LArMCTreeName_.c_str());
-    
+
     GetParameter("PhotonSummarySeriesName", PhotonSummarySeriesName_);
     log_info("+ PhotonSummarySeries : %s", PhotonSummarySeriesName_.c_str());
 
     // initialize injector and response services
-    injector_->Configure();
-    response_->Configure();
+    //injector_->Configure();
+    //response_->Configure();
     response_->Initialize();
 
     // set our controls over SD
@@ -96,77 +96,158 @@ void CCMSimulator::DetectorStatus(I3FramePtr frame) {
     PushFrame(frame);
 }
 
+void CCMSimulator::Simulation(I3FramePtr frame) {
+    seen_s_frame_ = true;
+    FillSimulationFrame(frame);
+    PushFrame(frame);
+}
+
+void CCMSimulator::FillSimulationFrame(I3FramePtr frame) {
+    // put mcTree into frame
+    I3FrameObjectPtr obj = injector_->GetSimulationConfiguration();
+    frame->Put("InjectorConfiguration", obj);
+}
+
 void CCMSimulator::DAQ(I3FramePtr frame) {
+    if(not seen_s_frame_) {
+        I3FramePtr sim_frame = boost::make_shared<I3Frame>(I3Frame::Simulation);
+        FillSimulationFrame(sim_frame);
+        PushFrame(sim_frame);
+        seen_s_frame_ = true;
+    }
+
     log_debug("   Simulating CCM");
-    
+
     // let's grab the mcPrimary from the injector
-    mcTree_ = injector_->GetMCTree();
-    
-    std::vector<I3Particle*> primary_particles = I3MCTreeUtils::GetPrimariesPtr(mcTree_);
-    
-    for (size_t p = 0; p < primary_particles.size(); p++){
+    I3MCTreePtr injection_tree = injector_->GetMCTree();
+    frame->Put("I3MCTree", injection_tree);
+
+    I3MCTreePtr edep_tree = boost::make_shared<I3MCTree>(*injection_tree);
+    CCMMCPESeriesMapPtr mcpeseries_map = boost::make_shared<CCMMCPESeriesMap>();
+    PhotonSummarySeriesPtr AllPhotonSummarySeries;
+    boost::shared_ptr<I3Map<int, size_t>> AllPhotonSummaryMap;
+    size_t max_id = 0;
+
+    // Iterate over all particles in the MCTree
+    typename I3MCTree::fast_const_iterator tree_iter(*injection_tree), tree_end=injection_tree->cend_fast();
+    for(;tree_iter != tree_end; tree_iter++) {
+        I3Particle const & particle = *tree_iter;
+
         // Tell the response service of a new event
-        response_->BeginEvent(*primary_particles[p]);
+        response_->BeginEvent(particle);
 
         // Tell the response service that the event has ended
         // this will also populate the map between CCMPMTKey and std::vector<CCMMCPE> to save to frame
         // also grab hits in fiducial argon for voxelization
         // note -- if SD not enabled, these will just be empty objects
-        I3MCTreePtr LArEnergyDep = boost::make_shared<I3MCTree>();
-        boost::shared_ptr<CCMMCPESeriesMap> CCMMCPEMap = boost::make_shared<CCMMCPESeriesMap>();
-        PhotonSummarySeriesPtr photon_summary_series = boost::make_shared<PhotonSummarySeries>();
-        boost::shared_ptr<I3Map<int, size_t>> photon_summary_series_map = boost::make_shared<I3Map<int, size_t>>(); 
-        
+
+        // The following are uninitialized because they get passed by reference to the response service
+        // and are overwritten there
+        I3MCTreePtr LArEnergyDep;
+        boost::shared_ptr<CCMMCPESeriesMap> CCMMCPEMap;
+        PhotonSummarySeriesPtr photon_summary_series;
+        boost::shared_ptr<I3Map<int, size_t>> photon_summary_series_map;
+
         response_->EndEvent(LArEnergyDep, CCMMCPEMap, photon_summary_series, photon_summary_series_map);
 
-        // now save to put into frames and push at the end 
-        AllEventsLArEnergyDep.push_back(boost::make_shared<I3MCTree>(*LArEnergyDep));
-        AllEventsCCMMCPEMap.push_back(boost::make_shared<CCMMCPESeriesMap>(*CCMMCPEMap));
-        AllPhotonSummarySeries.push_back(boost::make_shared<PhotonSummarySeries>(*photon_summary_series));
-        AllPhotonSummaryMap.push_back(boost::make_shared<I3Map<int, size_t>>(*photon_summary_series_map));
+        // Order of these matters
+        // Recursively add energy depositions from LArEnergyDep to tree
+        AppendSubTree(edep_tree, particle.GetID(), LArEnergyDep);
+        // Update photon summary map with new parent/track ids and offset indices
+        AppendPhotonSummaryMap(AllPhotonSummaryMap, photon_summary_series_map, max_id, AllPhotonSummarySeries->size());
+        // Append photon summary series
+        AppendPhotonSummarySeries(AllPhotonSummarySeries, particle, photon_summary_series);
+        // Append MCPE map, updating time, parent_id, and track_id
+        AppendMCPEMap(mcpeseries_map, particle, CCMMCPEMap, max_id);
     }
 
-    // terminate geant4    
-    response_->TerminateRun();
+    // sort mcpeseries_map by time
+    for (CCMMCPESeriesMap::iterator it = mcpeseries_map->begin(); it != mcpeseries_map->end(); ++it) {
+        std::sort(it->second.begin(), it->second.end(), [](const CCMMCPE& a, const CCMMCPE& b) { return a.g4_time < b.g4_time; });
+    }
+
+    frame->Put(PMTHitSeriesName_, mcpeseries_map);
+    frame->Put(LArMCTreeName_, edep_tree);
+    frame->Put(PhotonSummarySeriesName_, AllPhotonSummarySeries);
+    frame->Put("PhotonSummaryMap", AllPhotonSummaryMap);
+
+    PushFrame(frame);
 }
 
-
-void CCMSimulator::Finish(){
-    
-    // now save simulation set up info (just MC tree for now) into S frame
-    I3FramePtr simframe(new I3Frame(I3Frame::Simulation));
-    // put mcTree into frame
-    simframe->Put(mcPrimaryName_, mcTree_);
-    // push simframe
-    PushFrame(simframe);
-
-    // now go through our deque,
-    // save information for each event to frames,
-    // pop from deque, and push frame
-    while (AllEventsLArEnergyDep.size() > 0){
-        I3FramePtr tempframe(new I3Frame(I3Frame::DAQ));
-        
-        tempframe->Put(PMTHitSeriesName_, AllEventsCCMMCPEMap.at(0));
-        // remove CCMMCPEMap from cache
-        AllEventsCCMMCPEMap.pop_front();
-        
-        tempframe->Put(LArMCTreeName_, AllEventsLArEnergyDep.at(0));
-        // remove LArMCTree from cache
-        AllEventsLArEnergyDep.pop_front();
-        
-        tempframe->Put(PhotonSummarySeriesName_, AllPhotonSummarySeries.at(0));
-        // remove photon summary from cache
-        AllPhotonSummarySeries.pop_front();
-        
-        tempframe->Put("PhotonSummaryMap", AllPhotonSummaryMap.at(0));
-        // remove photon summary from cache
-        AllPhotonSummaryMap.pop_front();
-        
-        PushFrame(tempframe);
+void CCMSimulator::AppendSubTree(I3MCTreePtr tree, I3ParticleID primary_id, I3MCTreeConstPtr subtree) {
+    // Append the subtree to the tree
+    std::vector<const I3Particle*> subtree_primaries = I3MCTreeUtils::GetPrimariesPtr(subtree);
+    std::vector<const I3Particle*> subtree_daughters;
+    for (const I3Particle* primary : subtree_primaries) {
+        I3MCTreeUtils::AppendChild(*tree, primary_id, *primary);
+        std::vector<const I3Particle*> daughters = I3MCTreeUtils::GetDaughtersPtr(subtree, primary->GetID());
+        subtree_daughters.insert(subtree_daughters.end(), daughters.begin(), daughters.end());
     }
-   
-   // destruct g4 interface
-   response_->DestroyInterface(); 
+    subtree_primaries = subtree_daughters;
+    subtree_daughters.clear();
+    while(subtree_primaries.size() > 0) {
+        for (const I3Particle* primary : subtree_primaries) {
+            I3Particle const * parent = I3MCTreeUtils::GetParentPtr(subtree, primary->GetID());
+            I3MCTreeUtils::AppendChild(*tree, parent->GetID(), *primary);
+            std::vector<const I3Particle*> daughters = I3MCTreeUtils::GetDaughtersPtr(subtree, primary->GetID());
+            subtree_daughters.insert(subtree_daughters.end(), daughters.begin(), daughters.end());
+        }
+        subtree_primaries = subtree_daughters;
+        subtree_daughters.clear();
+    }
+}
+
+void CCMSimulator::AppendPhotonSummaryMap(boost::shared_ptr<I3Map<int, size_t>> parent_map, boost::shared_ptr<I3Map<int, size_t>> sub_map, size_t max_id, size_t offset) {
+    for(I3Map<int, size_t>::const_iterator it = sub_map->begin(); it != sub_map->end(); ++it) {
+        parent_map->insert(std::make_pair(it->first + max_id, it->second + offset));
+    }
+}
+
+void CCMSimulator::AppendPhotonSummarySeries(PhotonSummarySeriesPtr parent_series, I3Particle const & parent_particle, PhotonSummarySeriesConstPtr sub_series) {
+    double parent_time = parent_particle.GetTime();
+
+    parent_series->reserve(parent_series->size() + sub_series->size());
+    for(PhotonSummary const & ps : *sub_series) {
+        parent_series->push_back(ps);
+        parent_series->back().g4_time += parent_time;
+    }
+}
+
+void CCMSimulator::AppendMCPEMap(CCMMCPESeriesMapPtr parent_map, I3Particle const & parent_particle, CCMMCPESeriesMapConstPtr sub_map, size_t & max_id) {
+
+    size_t sub_map_max_id = 0;
+    for(CCMMCPESeriesMap::const_iterator it = sub_map->begin(); it != sub_map->end(); ++it) {
+        for(CCMMCPE const & mpe : it->second) {
+            sub_map_max_id = std::max(sub_map_max_id, mpe.parent_id);
+            sub_map_max_id = std::max(sub_map_max_id, mpe.track_id);
+        }
+    }
+
+    double parent_time = parent_particle.GetTime();
+    for (CCMMCPESeriesMap::const_iterator it = sub_map->begin(); it != sub_map->end(); ++it) {
+        CCMMCPESeriesMap::iterator parent_it = parent_map->find(it->first);
+        if (parent_it == parent_map->end()) {
+            parent_map->insert(*it);
+        } else {
+            parent_it->second.reserve(parent_it->second.size() + it->second.size());
+            for(CCMMCPE const & mpe : it->second) {
+                parent_it->second.push_back(mpe);
+                CCMMCPE & p = parent_it->second.back();
+                p.g4_time += parent_time;
+                p.parent_id += max_id;
+                p.track_id += max_id;
+            }
+        }
+    }
+
+    max_id += sub_map_max_id + 1;
+}
+
+void CCMSimulator::Finish() {
+    // terminate geant4
+    response_->TerminateRun();
+    // destruct g4 interface
+    response_->DestroyInterface();
 }
 
 
--- projects/g4-larsim/public/g4-larsim/CCMSimulator.h
+++ projects/g4-larsim/public/g4-larsim/CCMSimulator.h
@@ -26,6 +26,8 @@ class CCMSimulator : public I3Module  {
         void Configure();
         void DetectorStatus(I3FramePtr frame);
         void DAQ(I3FramePtr frame);
+        void Simulation(I3FramePtr frame);
+        void FillSimulationFrame(I3FramePtr frame);
         void Finish();
     private:
   
@@ -35,15 +37,16 @@ class CCMSimulator : public I3Module  {
         std::string PMTHitSeriesName_;
         std::string LArMCTreeName_; 
         std::string PhotonSummarySeriesName_; 
-        I3MCTreePtr mcTree_;
+
+        bool seen_s_frame_ = false;
 
         CCMParticleInjectorPtr injector_;
         CCMDetectorResponsePtr response_;
 
-        std::deque<boost::shared_ptr<CCMMCPESeriesMap>> AllEventsCCMMCPEMap;
-        std::deque<I3MCTreePtr> AllEventsLArEnergyDep;
-        std::deque<PhotonSummarySeriesPtr> AllPhotonSummarySeries;
-        std::deque<boost::shared_ptr<I3Map<int, size_t>>> AllPhotonSummaryMap;
+        static void AppendSubTree(I3MCTreePtr tree, I3ParticleID primary_id, I3MCTreeConstPtr subtree);
+        static void AppendPhotonSummaryMap(boost::shared_ptr<I3Map<int, size_t>> parent_map, boost::shared_ptr<I3Map<int, size_t>> sub_map, size_t max_id, size_t offset);
+        static void AppendPhotonSummarySeries(PhotonSummarySeriesPtr parent_series, I3Particle const & parent_particle, PhotonSummarySeriesConstPtr sub_series);
+        static void AppendMCPEMap(CCMMCPESeriesMapPtr parent_map, I3Particle const & parent_particle, CCMMCPESeriesMapConstPtr sub_map, size_t & max_id);
 
         bool PMTSDStatus_; // turn PMT SD on/off
         bool LArSDStatus_; // turn fiducial LAr SD on/off
