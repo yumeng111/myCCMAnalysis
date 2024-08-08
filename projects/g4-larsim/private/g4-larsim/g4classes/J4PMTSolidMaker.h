8904a9444ebf53fdde45b32abd64d7a235d73c24
Austin Schneider <hogenshpogen@gmail.com>
2024-08-07 18:43:53 -0600

    Intersect solids with tubs

--- projects/g4-larsim/private/g4-larsim/g4classes/J4PMTSolidMaker.h
+++ projects/g4-larsim/private/g4-larsim/g4classes/J4PMTSolidMaker.h
@@ -21,10 +21,14 @@ class J4PMTSolidMaker {
 
   public:
 
-
     static void Create8inchPMTSolid();
     static void CreateTPBCoatingSolid(); 
     static void CreatePhotocathodeSolid(); 
+
+    static void Create8inchPMTCapsSolid(G4double protrusion_distance);
+    static void Create8inchPMTWallSolid(G4double cylinder_radius, G4double protrusion_distance);
+    static void CreateTPBCoatingCapsSolid(G4double protrusion_distance);
+    static void CreateTPBCoatingWallSolid(G4double cylinder_radius, G4double protrusion_distance);
 
     static G4VSolid * Get8inchPMTSolid();
     static G4VSolid * GetTPBCoatingSolid();
@@ -34,14 +38,33 @@ class J4PMTSolidMaker {
     static G4double GetTPBThickness() { return fTPBThickness; }
     static G4double GetPhotocathodeThickness() { return fPhotocathodeDelta; }
 
+    static G4VSolid * Get8inchPMTCapsSolid(G4double protrusion_distance);
+    static G4VSolid * Get8inchPMTWallSolid(G4double cylinder_radius, G4double protrusion_distance);
+    static G4VSolid * GetTPBCoatingCapsSolid(G4double protrusion_distance);
+    static G4VSolid * GetTPBCoatingWallSolid(G4double cylinder_radius, G4double protrusion_distance);
+
   private:
 
-    static G4VSolid * f8inchPMTSolid;
-    static G4VSolid * fTPBCoatingSolid;
-    static G4VSolid * fPhotocathodeSolid;
-    
+
     static G4double f8inchPMTRadius;
     static G4double fTPBThickness;
+    static G4double fCylinderRadius;
+    static G4double fPMTProtrusionDistance;
+
+    static G4VSolid * fTubSolid;
+    static G4VSolid * fBoxSolid;
+
+    static G4VSolid * f8inchPMTSolid;
+
+    static G4VSolid * f8inchPMTCapsSolid;
+    static G4VSolid * f8inchPMTWallSolid;
+
+    static G4VSolid * fTPBCoatingSolid;
+    static G4VSolid * fTPBCoatingCapsSolid;
+    static G4VSolid * fTPBCoatingWallSolid;
+
+
+    static G4VSolid * fPhotocathodeSolid;
     static G4double fPhotocathodeDelta;
 
 };
