bacc653a8391a91a073358b151e34eddc4e2cbbd
Austin Schneider <hogenshpogen@gmail.com>
2024-08-18 14:53:02 -0600

    Stack-trace for g4-larsim

---
+++ projects/g4-larsim/private/g4-larsim/StackTrace.cxx
@@ -0,0 +1,1 @@
+#include <g4-larsim/StackTrace.h>
---
+++ projects/g4-larsim/public/g4-larsim/StackTrace.h
@@ -0,0 +1,38 @@
+#ifndef g4_larsim_StackTrace_H
+#define g4_larsim_StackTrace_H
+
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <dlfcn.h>
+#include <cxxabi.h>
+#include <iostream>
+#include <typeinfo>
+#include <execinfo.h>
+#include <cpptrace/cpptrace.hpp>
+
+namespace {
+    void * last_frames[20];
+    size_t last_size;
+    std::string exception_name;
+
+    std::string demangle(const char *name) {
+        int status;
+        std::unique_ptr<char,void(*)(void*)> realname(abi::__cxa_demangle(name, 0, 0, &status), &std::free);
+        return status ? "failed" : &*realname;
+    }
+}
+
+extern "C" {
+    void __cxa_throw(void *ex, void *info, void (*dest)(void *)) {
+        exception_name = demangle(reinterpret_cast<const std::type_info*>(info)->name());
+        last_size = backtrace(last_frames, sizeof last_frames/sizeof(void*));
+
+        cpptrace::generate_trace().print();
+
+        static void (*const rethrow)(void*,void*,void(*)(void*)) __attribute__ ((noreturn)) = (void (*)(void*,void*,void(*)(void*)))dlsym(RTLD_NEXT, "__cxa_throw");
+        rethrow(ex,info,dest);
+    }
+}
+
+#endif // g4_larsim_StackTrace_H
