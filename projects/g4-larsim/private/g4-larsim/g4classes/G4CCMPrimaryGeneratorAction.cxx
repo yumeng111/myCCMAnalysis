cc3d1b5c9d9ceca24be9425f4f45be50c8f81983
Austin Schneider <hogenshpogen@gmail.com>
2024-09-08 20:03:50 -0600

    Primary generator action and particle list

--- projects/g4-larsim/private/g4-larsim/g4classes/G4CCMPrimaryGeneratorAction.cxx
+++ projects/g4-larsim/private/g4-larsim/g4classes/G4CCMPrimaryGeneratorAction.cxx
@@ -1,34 +1,6 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-/// \file optical/LXe/src/LXePrimaryGeneratorAction.cc
-/// \brief Implementation of the LXePrimaryGeneratorAction class
-//
-//
-#include "G4CCMPrimaryGeneratorAction.h"
+#include <cmath>
+
+#include "g4-larsim/G4CCMPrimaryGeneratorAction.h"
 
 #include "globals.hh"
 #include "G4Event.hh"
@@ -36,51 +8,266 @@
 #include "G4ParticleGun.hh"
 #include "G4ParticleTable.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4Geantino.hh"
 #include "G4IonTable.hh"
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-G4CCMPrimaryGeneratorAction::G4CCMPrimaryGeneratorAction(){
-    G4int n_particle = 1;
-    fParticleGun = new G4ParticleGun(n_particle);
-
-    G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-    G4ParticleDefinition *particle = particleTable->FindParticle("geantino");
-
-    G4double inset = 0.25 * cm;
-    G4ThreeVector pos(0.,0.,0. + inset);
-    G4ThreeVector mom(0.,0.,-1.);
-
-    fParticleGun->SetParticlePosition(pos);
-    fParticleGun->SetParticleMomentumDirection(mom);
-    fParticleGun->SetParticleMomentum(0.*GeV);
-    fParticleGun->SetParticleDefinition(particle);
-
+G4CCMParticleList::G4CCMParticleList() : particle_list_(), current_particle_ (0) {
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-G4CCMPrimaryGeneratorAction::~G4CCMPrimaryGeneratorAction() { 
-    delete fParticleGun; 
-    //delete fGeneralParticleSource;
+void G4CCMParticleList::AddParticle(I3Particle p) {
+    particle_list_.push_back(p);
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void G4CCMParticleList::AddParticles(std::vector<I3Particle> p) {
+    particle_list_.insert(particle_list_.end(), p.begin(), p.end());
+}
 
-void G4CCMPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
-    if (fParticleGun->GetParticleDefinition() == G4Geantino::Geantino()) {  
+void G4CCMParticleList::SetParticles(std::vector<I3Particle> p) {
+    particle_list_ = p;
+    current_particle_ = 0;
+}
+
+void G4CCMParticleList::Clear() {
+    particle_list_.clear();
+    current_particle_ = 0;
+}
+
+I3Particle G4CCMParticleList::GetNextParticle() {
+    std::lock_guard<std::mutex> const lock(mutex);
+    if(current_particle_ >= particle_list_.size())
+        return I3Particle();
+    if(current_particle_ >= particle_list_.size())
+        log_fatal("current_particle_ >= particle_list_.size()");
+    return particle_list_[current_particle_++];
+}
+
+G4CCMPrimaryGeneratorAction::G4CCMPrimaryGeneratorAction(G4CCMParticleList * particle_list) {
+    particle_list_ = particle_list;
+}
+
+void G4CCMPrimaryGeneratorAction::GeneratePrimaries(G4Event * evt) {
+    I3Particle p = particle_list_->GetNextParticle();
+
+    if( std::isnan(p.GetTime()) ) {
+        p.SetTime(0.);
+    }
+
+    // Create a new vertex
+    //
+    auto* vertex =
+        new G4PrimaryVertex(particle_position,particle_time);
+
+    // Create new primaries and set them to the vertex
+    //
+    G4double mass =  particle_definition->GetPDGMass();
+    for( G4int i=0; i<NumberOfParticlesToBeGenerated; ++i )
+    {
+        auto* particle =
+            new G4PrimaryParticle(particle_definition);
+        particle->SetKineticEnergy( particle_energy );
+        particle->SetMass( mass );
+        particle->SetMomentumDirection( particle_momentum_direction );
+        particle->SetCharge( particle_charge );
+        particle->SetPolarization(particle_polarization.x(),
+                particle_polarization.y(),
+                particle_polarization.z());
+        particle->SetWeight( particle_weight );
+        vertex->SetPrimary( particle );
+    }
+    evt->AddPrimaryVertex( vertex );
+}
+
+G4PrimaryVertex * CreatePrimaryVertex(I3Particle const & primary) {
+
+
+    G4Double x = (particle.GetX() / I3Units::m) * CLHEP::m;
+    G4Double y = (particle.GetY() / I3Units::m) * CLHEP::m;
+    G4Double z = (particle.GetZ() / I3Units::m) * CLHEP::m;
+
+    G4ThreeVector direction(particle.GetDir().GetX(),
+                            particle.GetDir().GetY(),
+                            particle.GetDir().GetZ());
+
+    G4Double time = particle.GetTime() / I3Units::ns * CLHEP::ns;
+
+    G4PrimaryVertex * vertex = new G4PrimaryVertex(x, y, z, time);
+    G4ParticleTable * particleTable = G4ParticleTable::GetParticleTable();
+    G4ParticleDefinition * particleDefinition = GetParticleDefinition(particle);
+
+    G4Double mass = particleDefinition->GetPDGMass();
+    G4Double energy = primary.GetEnergy() / I3Units::MeV * CLHEP::MeV;
+
+    G4PrimaryParticle * particle = new G4PrimaryParticle(particleDefinition);
+
+    particle->SetMass(mass);
+    particle->SetKineticEnergy(energy);
+    particle->SetMomentumDirection(momentum_dir);
+    particle->SetCharge(primary.GetCharge());
+
+    if(particle.GetType() == I3Particle::Na22Nucleus) {
+        particle->SetCharge(0.);
+    }
+
+    vertex->SetPrimary(particle);
+
+    log_trace("Injecting %s: x=%.2f m, y=%.2f m, z=%.2f m, E=%.3f MeV",
+              particle.GetTypeString().c_str(),
+              position.x() / CLHEP::m,
+              position.y() / CLHEP::m,
+              position.z() / CLHEP::m,
+              particle.GetEnergy() / CLHEP::MeV);
+
+    return vertex;
+}
+
+G4ParticleDefinition * GetParticleDefinition(I3Particle const & particle) {
+    G4ParticleTable * particleTable = G4ParticleTable::GetParticleTable();
+    G4ParticleDefinition* particleDef = NULL;
+
+    switch(particle.GetType()) {
+    case I3Particle::Gamma:
+       particleDef = particleTable->FindParticle("opticalphoton");
+       break;
+    case I3Particle::EMinus:
+       particleDef = particleTable->FindParticle("e-");
+       break;
+    case I3Particle::EPlus:
+       particleDef = particleTable->FindParticle("e+");
+       break;
+    case I3Particle::MuMinus:
+       particleDef = particleTable->FindParticle("mu-");
+       break;
+    case I3Particle::MuPlus:
+       particleDef = particleTable->FindParticle("mu+");
+       break;
+    case I3Particle::PPlus:
+       particleDef = particleTable->FindParticle("proton");
+       break;
+    case I3Particle::PMinus:
+       particleDef = particleTable->FindParticle("anti_proton");
+       break;
+    case I3Particle::Neutron:
+       particleDef = particleTable->FindParticle("neutron");
+       break;
+#ifdef I3PARTICLE_SUPPORTS_PDG_ENCODINGS
+    case I3Particle::NeutronBar:
+#else
+    case 25:
+#endif
+       particleDef = particleTable->FindParticle("anti_neutron");
+       break;
+    case I3Particle::PiPlus:
+       particleDef = particleTable->FindParticle("pi+");
+       break;
+    case I3Particle::PiMinus:
+       particleDef = particleTable->FindParticle("pi-");
+       break;
+    case I3Particle::Pi0:
+       particleDef = particleTable->FindParticle("pi0");
+       break;
+    case I3Particle::KPlus:
+       particleDef = particleTable->FindParticle("kaon+");
+       break;
+    case I3Particle::KMinus:
+       particleDef = particleTable->FindParticle("kaon-");
+       break;
+    case I3Particle::K0_Long:
+       particleDef = particleTable->FindParticle("kaon0L");
+       break;
+    case I3Particle::K0_Short:
+       particleDef = particleTable->FindParticle("kaon0S");
+       break;
+    case I3Particle::NuE:
+       particleDef = particleTable->FindParticle("nu_e");
+       break;
+    case I3Particle::NuEBar:
+       particleDef = particleTable->FindParticle("anti_nu_e");
+       break;
+    case I3Particle::NuMu:
+       particleDef = particleTable->FindParticle("nu_mu");
+       break;
+    case I3Particle::NuMuBar:
+       particleDef = particleTable->FindParticle("anti_nu_mu");
+       break;
+    case I3Particle::NuTau:
+       particleDef = particleTable->FindParticle("nu_tau");
+       break;
+    case I3Particle::NuTauBar:
+       particleDef = particleTable->FindParticle("anti_nu_tau");
+       break;
+    case I3Particle::Lambda:
+       particleDef = particleTable->FindParticle("lambda");
+       break;
+    //~ new particles added!!!
+    case I3Particle::LambdaBar:
+       particleDef = particleTable->FindParticle("anti_lambda");
+       break;
+    case I3Particle::SigmaMinusBar:
+       particleDef = particleTable->FindParticle("anti_sigma+");
+       break;
+    case I3Particle::Xi0Bar:
+       particleDef = particleTable->FindParticle("anti_xi0");
+       break;
+    case I3Particle::Xi0:
+       particleDef = particleTable->FindParticle("xi0");
+       break;
+     case I3Particle::SigmaPlus:
+       particleDef = particleTable->FindParticle("sigma+");
+       break;
+    case I3Particle::SigmaMinus:
+       particleDef = particleTable->FindParticle("sigma-");
+       break;
+    case I3Particle::XiMinus:
+       particleDef = particleTable->FindParticle("xi-");
+       break;
+    case I3Particle::SigmaPlusBar:
+       particleDef = particleTable->FindParticle("anti_sigma-");
+       break;
+    case I3Particle::XiPlusBar:
+       particleDef = particleTable->FindParticle("anti_xi-");
+       break;
+    case I3Particle::OmegaPlusBar:
+       particleDef = particleTable->FindParticle("anti_omega-");
+       break;
+    case I3Particle::H2Nucleus:
+       particleDef = particleTable->FindParticle("deuteron");
+       break;
+    case I3Particle::H3Nucleus:
+       particleDef = particleTable->FindParticle("triton");
+       break;
+    case I3Particle::He3Nucleus:
+       particleDef = particleTable->FindParticle("He3");
+       break;
+    case I3Particle::He4Nucleus:
+       particleDef = particleTable->FindParticle("He4");
+       break;
+    case I3Particle::He5Nucleus:
+       particleDef = particleTable->FindParticle("He5");
+       break;
+    case I3Particle::He6Nucleus:
+       particleDef = particleTable->FindParticle("He6");
+       break;
+    case I3Particle::Na22Nucleus:
+       sodium_run = true;
+       break;
+    default:
+      log_warn("Man, check out that strange particle \"%s\" ?!", particle.GetTypeString().c_str());
+      return;
+    }
+
+    // special logic for adding sodium particle
+    if(sodium_run) {
         G4int Z = 11, A = 22;
         G4double ionCharge   = 0.*eplus;
         G4double excitEnergy = 0.*keV;
 
-        G4ParticleDefinition* ion
-           = G4IonTable::GetIonTable()->GetIon(Z,A,excitEnergy);
-        fParticleGun->SetParticleDefinition(ion);
-        fParticleGun->SetParticleCharge(ionCharge);
-    }    
-    //create vertex
-    //   
-    fParticleGun->GeneratePrimaryVertex(anEvent);
+        particleDef = G4IonTable::GetIonTable()->GetIon(Z,A,excitEnergy);
+    }
+
+    if(!particleDef) {
+        log_warn("You passed NULL particleDef \"%s\" ?!", particle.GetTypeString().c_str());
+        return;
+    }
+
+    return particleDef;
 }
 
--- projects/g4-larsim/private/g4-larsim/g4classes/G4CCMPrimaryGeneratorAction.h
+++ projects/g4-larsim/private/g4-larsim/g4classes/G4CCMPrimaryGeneratorAction.h
@@ -1,58 +1,41 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-/// \file optical/LXe/include/LXePrimaryGeneratorAction.hh
-/// \brief Definition of the LXePrimaryGeneratorAction class
-//
-//
 #ifndef G4CCMPrimaryGeneratorAction_h
-#define G4CCMPrimaryGeneratorAction_h 1
+#define G4CCMPrimaryGeneratorAction_h
+
+#include <mutex>
+#include <vertex>
+
+#include "dataclasses/physics/I3Particle.h"
 
 #include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4GeneralParticleSource.hh"
 
-class G4ParticleGun;
 class G4Event;
 
-class G4CCMPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
-{
- public:
-  G4CCMPrimaryGeneratorAction();
-  ~G4CCMPrimaryGeneratorAction() override;
+class G4CCMParticleList {
+    std::mutex mutex;
+    std::vector<I3Particle> particle_list_;
+    unsigned int current_particle_;
+public:
+    G4CCMParticleList();
+    ~G4CCMParticleList() = default;
 
-  void GeneratePrimaries(G4Event* anEvent) override;
-
-  // method to access particle gps
-  //const G4GeneralParticleSource *GetGeneralParticleSource() const { return fGeneralParticleSource; }
-  G4ParticleGun* GetParticleGun() { return fParticleGun;} ;
-
- private:
-  //G4GeneralParticleSource *fGeneralParticleSource; // pointer a to G4 GeneralParticleSource class
-  G4ParticleGun* fParticleGun = nullptr;
+    void AddParticle(I3Particle p);
+    void AddParticles(std::vector<I3Particle> p);
+    void SetParticles(std::vector<I3Particle> p);
+    void Clear();
+    I3Particle GetNextParticle();
 };
 
-#endif
+class G4CCMPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+    G4CCMParticleList * particle_list_;
+public:
+    G4CCMPrimaryGeneratorAction(G4CCMParticleList * particle_list);
+    ~G4CCMPrimaryGeneratorAction() = default;
+
+    void GeneratePrimaries(G4Event* anEvent) override;
+};
+
+G4PrimaryVertex * CreatePrimaryVertex(I3Particle const & primary);
+G4ParticleDefinition * GetParticleDefinition(I3Particle const & primary);
+
+#endif // G4CCMPrimaryGeneratorAction_h
 
