5da87a2fab4690a28959daf4d5b5b4b04b0f929d
Austin Schneider <hogenshpogen@gmail.com>
2025-08-01 23:13:00 -0600

    MAINT: docstring and __all__

--- projects/marley/python/__init__.py
+++ projects/marley/python/__init__.py
@@ -1,6 +1,10 @@
+"""Python helpers for MARLEY integration."""
+
 from icecube import icetray
 from . import NuESIRENMarleyInjector
 
 NuESIRENMarleyInjector = NuESIRENMarleyInjector.NuESIRENMarleyInjector
+
+__all__ = ["NuESIRENMarleyInjector"]
 
 del icetray
