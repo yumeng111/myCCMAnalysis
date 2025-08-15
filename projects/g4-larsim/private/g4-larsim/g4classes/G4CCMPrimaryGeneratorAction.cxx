53cf4f70831324f1de91eac4e25274a3f4dd29b8
Marisol Chavez Estrada <fismarisol@gmail.com>
2025-08-15 17:57:31 -0400

    remove comment

--- projects/g4-larsim/private/g4-larsim/g4classes/G4CCMPrimaryGeneratorAction.cxx
+++ projects/g4-larsim/private/g4-larsim/g4classes/G4CCMPrimaryGeneratorAction.cxx
@@ -106,7 +106,7 @@ G4ParticleDefinition * GetParticleDefinition(I3Particle const & particle) {
 
     switch(particle.GetType()) {
     case I3Particle::Gamma:
-       particleDef = particleTable->FindParticle("gamma"); //originally was opticalphoton
+       particleDef = particleTable->FindParticle("gamma");
        break;
     case I3Particle::EMinus:
        particleDef = particleTable->FindParticle("e-");
