493525b38837dd2afd78a1b8c1139fa16367206a
Austin Schneider <hogenshpogen@gmail.com>
2025-10-21 14:16:21 -0500

    Spelling

--- projects/dataclasses/private/pybindings/I3Maps/CCMRecoPulseSeriesMapApplyOffsets.cxx
+++ projects/dataclasses/private/pybindings/I3Maps/CCMRecoPulseSeriesMapApplyOffsets.cxx
@@ -14,7 +14,7 @@ void register_CCMRecoPulseSeriesMapApplyOffsets()
 {
   bp::class_<CCMRecoPulseSeriesMapApplyOffsets, bp::bases<I3FrameObject>,
       CCMRecoPulseSeriesMapApplyOffsetsPtr>("CCMRecoPulseSeriesMapApplyOffsets",
-      bp::init<std::string const &, std::string const &, std::string const &, double const &>(bp::args("pulses_key", "orignal_offsets_key", "target_offsets_key", "mod")))
+      bp::init<std::string const &, std::string const &, std::string const &, double const &>(bp::args("pulses_key", "original_offsets_key", "target_offsets_key", "mod")))
     .def("apply", &underhanded_apply, "Apply the correction to an I3Frame, returning an CCMRecoPulseSeries.")
     .add_property("pulses_source", &CCMRecoPulseSeriesMapApplyOffsets::GetPulsesSource)
     .add_property("original_offsets_source", &CCMRecoPulseSeriesMapApplyOffsets::GetOriginalOffsetsSource)
